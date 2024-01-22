ef7b98aec1ca29d9562fb30334ff034e121672e4
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-22 15:52:53 -0500

    Initial commit. Will be a modal that prompts the user for a new game.

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GameOver = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default GameOver
---
+++ src/components/GameWin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GameWin = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default GameWin
