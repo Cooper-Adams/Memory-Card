85a40dcef4cf60c463f0fc154510b9c9df9fa2f0
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-21 22:50:48 -0500

    Initial commit

---
+++ src/components/Characters.js
@@ -0,0 +1,126 @@
+import { nanoid } from 'nanoid'
+
+const Characters = [
+    {
+        id: nanoid(),
+        name: 'Super Saiyan Goku & Super Saiyan Vegeta & Super Saiyan Trunks (Teen)',
+        title: 'All-Out Ultimate Battle',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1028060/1028060.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan 3 Goku & Super Saiyan 2 Vegeta',
+        title: "Universe's Last Hope",
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1025730/1025730.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan 4 Goku',
+        title: 'Apex of Supreme Saiyan Power',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1014210/1014210.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Android #17 (Team Universe 7)',
+        title: "Universe 7's All-Out Offensive",
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1021480/1021480.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan Trunks (Future)',
+        title: 'Hope at the End of a Deadly Showdown',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/4027300/4027300.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Goku (Ultra Instinct -Sign-)',
+        title: 'Sign of a Turnaround',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1019820/1019820.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan 2 Gohan (Youth)',
+        title: 'Awakened True Power',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/4017640/4017640.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan Gohan (Future)',
+        title: 'Indomitable Fighter',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1027330/1027330.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan God SS Vegito',
+        title: 'All-Out Final Battle',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/4027130/4027130.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Orange Piccolo (Giant Form)',
+        title: 'Exploding Latent Power',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/4025590/4025590.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Bulma (Youth)',
+        title: 'A Quest That Makes Wishes Come True',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1026610/1026610.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan God SS Goku & Super Saiyan God SS Vegeta',
+        title: 'Divine Warriors with Infinite Power',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/4022390/4022390.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan Trunks (Future)',
+        title: 'Alternate Ending',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1014050/1014050.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan Vegeta (GT)',
+        title: 'Miracle-Making Final Showdown',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/4025780/4025780.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan God SS Vegito',
+        title: 'All or Nothing',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1011680/1011680.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Ultimate Gohan',
+        title: 'Awakened Super Hero',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1025560/1025560.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan 3 Goku',
+        title: 'Golden Fist',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1012720/1012720.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Goku',
+        title: 'Light that Defends the Green Planet',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1026820/1026820.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan 4 Gogeta',
+        title: 'Saiyan Warriors with Ultimate Power',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/4022430/4022430.png'
+    },
+    {
+        id: nanoid(),
+        name: 'Super Saiyan God SS Evolved Vegeta',
+        title: 'True Warrior Race',
+        img: 'https://dokkaninfo.com/assets/japan/character/card/1020340/1020340.png'
+    },
+]
+
+export default Characters
