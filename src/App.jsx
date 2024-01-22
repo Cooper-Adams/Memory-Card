9c0c1828b5233995431cde873ac2a88940622db4
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-21 22:50:36 -0500

    Added bare framework for project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/header'
+
+function App() {
+    return (
+        <div className='react-app'>
+            <Header />
+            {/* <Game /> */}
+        </div>
+    )
+}
+
+export default App
