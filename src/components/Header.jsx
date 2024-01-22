28785c5daea373dfe0e78dfcb6f965ea8ddda955
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-22 15:52:25 -0500

    Fixed the Header so that it would understand props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function Header() {
+const Header = (props) => {
     return (
         <header className='header'>
-            <h1>Memory Cards</h1>
+            <h1 className='h-title'>Memory Cards</h1>
+            <div className='h-score-container'>
+                <span className='h-score'>Score: {props.score}</span>
+                <span className='h-highscore'>High Score: {props.highScore}</span>
+            </div>
         </header>
     )
 }
