d9350db0d03c4cc8c73ad874a5308974b1181d0e
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-22 15:49:22 -0500

    Built the card component to display the information of the card and handle being clicked

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Card() {
+const Card = (props) => {
+    const [wasClicked, setWasClicked] = useState(false)
+
+    const handleClick = () => {
+        if (wasClicked) {
+            props.setGameOver()
+        } else {
+            setWasClicked(true)
+            props.updateClickedCards({name: props.name, id: props.id})
+            props.updateScore()
+        }
+    }
+
     return (
-        <>
-        
-        </>
+        <div className='card' onClick={handleClick}>
+            <img className='card-img' src={props.img} />
+            <h4 className='card-name'>{props.name}</h4>
+            <h6 className='card-title'>{props.title}</h6>
+        </div>
     )
 }
 
