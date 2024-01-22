472edb6f0763dcb1d801a83ccece15ce765cef65
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-22 15:46:39 -0500

    Added the logic to track the score and defined the structure of the components to be displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/header'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+import GameWin from './components/GameWin'
 
-function App() {
+const App = (props) => {
+    const [score, setScore] = useState(0)
+    const [highScore, setHighScore] = useState(0)
+    const [gameOver, setGameOver] = useState(false)
+
+    /* Increments the current score */
+    const updateScore = () => { setScore(score + 1) }
+
+    /* Increments the high score once the first point is scored or if the current score surpasses the highest score */
+    const updateHighScore = () => {
+        if (highScore === 0 || score > highScore) {
+            setHighScore(score)
+        }
+    }
+
+    /* Ends the game state */
+    const gameFinished = () => { setGameOver(true) }
+
+    /* Resets the game, but not the high score */
+    const resetGame = () => {
+        setGameOver(false)
+        setScore(0)
+    }
+
+    /* Resets the game completely */
+    const resetGameWin = () => {
+        setScore(0)
+        setHighScore(0)
+        setGameOver(false)
+    }
+
+    /* Effect is used to update the high score, which is dependent on the current score changing */
+    useEffect(() => {
+        updateHighScore()
+    }, [score])
+
     return (
         <div className='react-app'>
-            <Header />
-            {/* <Game /> */}
+            <Header 
+                score={score}
+                highScore={highScore}
+            />
+            
+            <Game
+                updateScore={updateScore}
+                setGameOver={gameFinished}
+            />
+
+            {gameOver && <GameOver resetGame={resetGame} highScore={highScore} />}
+
+            {highScore === 20 && <GameWin resetGame={resetGameWin}/>}
         </div>
     )
 }
