e64c67c3b28caeadb4468a83b2678c8328f140e3
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-24 15:18:09 -0500

    Capitalized score text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = (props) => {
     return (
         <header className='header'>
-            <h2 className='h-title'>Memory Cards</h2>
+            <h2 className='h-title'>DOKKAN Memory Cards</h2>
             <div className='h-score-container'>
-                <h2 className='h-score'>Score: {props.score}</h2>
-                <h2 className='h-highscore'>High Score: {props.highScore}</h2>
+                <h2 className='h-score'>SCORE: {props.score}</h2>
+                <h2 className='h-highscore'>HIGH SCORE: {props.highScore}</h2>
             </div>
         </header>
     )
