7e6f266d57e1de14113c198ef91b39d8860fab30
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-27 18:43:19 -0500

    Added a separate character list for the sagas of the original Dragon Ball

---
+++ src/characters/dragonballsaga.js
@@ -0,0 +1,312 @@
+import { nanoid } from 'nanoid'
+
+const dbsaga = [
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (Youth)',
+            title: 'The Adventure Begins',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015730/1015730.png',
+            type: 'agl',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Yamcha & Puar',
+            title: 'Lupine Awakening',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015180/1015180.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Pilaf, Shu, & Mai',
+            title: 'The Secret Weapon of Confidence',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027580/1027580.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Bulma (Youth)',
+            title: 'A Quest That Makes Wishes Come True',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026610/1026610.png',
+            type: 'sstr',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (Youth)',
+            title: 'Encountering the Strongest Fighters',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1000270/1000270.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin (Youth)',
+            title: 'An Exhilirating Match',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027540/1027540.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Jackie Chun',
+            title: 'Climax of the World Tournament',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025630/1025630.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Namu',
+            title: 'Fearful Concentration',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005590/1005590.png',
+            type: 'int',
+            rarity: 'sr'
+        },
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (Youth)',
+            title: 'First Sighting of Snow',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020260/1020260.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Hacchan',
+            title: 'Greatest Rage and Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019680/1019680.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'General Blue',
+            title: 'Deep Blue Gaze',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002310/1002310.png',
+            type: 'int',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Mercenary Tao',
+            title: 'Bloodstained Sanctuary',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022340/1022340.png',
+            type: 'estr',
+            rarity: 'lr'
+        },
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Devilman',
+            title: 'Devilish Deception',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002360/1002360.png',
+            type: 'agl',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Dracula Man',
+            title: 'Unusual Battle Skills',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002380/1002380.png',
+            type: 'str',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Bandages',
+            title: 'Bone-Dry Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002370/1002370.png',
+            type: 'agl',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Grandpa Gohan',
+            title: 'Masked Master',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018000/1018000.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (Youth)',
+            title: 'Endless Challenge',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1013320/1013320.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Tenshinhan',
+            title: 'Secret Technique for Victory',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017440/1017440.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Chiaotzu',
+            title: 'Final Trick',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012560/1012560.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Yamcha',
+            title: 'Absolute Loss',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007320/1007320.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Demon King Piccolo (Elder)',
+            title: 'Massive Evil Shadow',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017760/1017760.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Cymbal',
+            title: "Demon King's Disciple",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1000480/1000480.png',
+            type: 'phy',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Tambourine',
+            title: 'Ghoulish Burst',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1000470/1000470.png',
+            type: 'str',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Drum',
+            title: "Terror's Descent",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1000490/1000490.png',
+            type: 'agl',
+            rarity: 'r'
+        },
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Demon King Piccolo (Elder)',
+            title: 'Overwhelming Power Restored',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4017790/4017790.png',
+            type: 'eteq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (Youth)',
+            title: 'All-Or-Nothing Punch',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016570/1016570.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Tenshinhan',
+            title: 'Last Resort for Victory',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019770/1019770.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Kami',
+            title: 'God-Guided Path',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011100/1011100.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Reunion in the Rain',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022260/1022260.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Chi-Chi',
+            title: 'Determined Love',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019930/1019930.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Cyborg Tao',
+            title: 'Bombardment of Accumulated Rage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026630/1026630.png',
+            type: 'estr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo Jr.',
+            title: 'A Flash of Hatred for Peace',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027190/1027190.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: "Showdown for the World's Strongest",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026530/1026530.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo Jr.',
+            title: 'Death Match for World Domination',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026560/1026560.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Tenshinhan',
+            title: 'The Might of Twelve Eyes',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021310/1021310.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin',
+            title: 'Tough All-Out Fight',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026580/1026580.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+    ]
+]
+
+export default dbsaga
