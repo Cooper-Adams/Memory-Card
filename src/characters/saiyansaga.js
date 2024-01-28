0c0ae2c0a6ebb0e8ed171600f23f84f85cb293fb
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-27 19:11:13 -0500

    Added a separate character list for the saiyan saga of Dragon Ball Z

---
+++ src/characters/saiyansaga.js
@@ -0,0 +1,210 @@
+import { nanoid } from 'nanoid'
+
+const saiyansaga = [
+    [
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)',
+            title: 'Courteous Son',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022790/1022790.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'A New Story Unfolds',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022760/1022760.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: "Demon King's Successor",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009210/1009210.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Raditz',
+            title: 'Shocking Arrival',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009140/1009140.png',
+            type: 'int',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Raditz',
+            title: 'Unveiling of Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024060/1024060.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku & Piccolo/Piccolo',
+            title: 'Desperate Onslaught',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024140/1024140.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)',
+            title: 'Power in a Pinch',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022810/1022810.png',
+            type: 'sstr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta & Nappa',
+            title: 'Threats Approaching from Outer Space',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028100/1028100.png',
+            type: 'teq',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Piccolo & Gohan (Kid)',
+            title: 'Strength Trained for a Year',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028120/1028120.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Tenshinhan',
+            title: 'Pre-Battle Jitters',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024710/1024710.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin',
+            title: 'Spirited Earth Defense',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019510/1019510.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Yamcha',
+            title: 'All-Out Decisive Fight',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025790/1025790.png',
+            type: 'int',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Budding Heart',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009230/1009230.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Tenshinhan',
+            title: 'The Last Tri-Beam',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024730/1024730.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin',
+            title: 'Raging Killer Move',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019530/1019530.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Yamcha',
+            title: 'Wounded Honor',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007340/1007340.png',
+            type: 'sagl',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Furious Counterattack',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022780/1022780.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)',
+            title: 'A Fierce Defense',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4028140/4028140.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Rampaging Vicious Saiyans',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4019410/4019410.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Nappa',
+            title: 'Overwhelming Assault',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019390/1019390.png',
+            type: 'eagl',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (Kaioken)',
+            title: 'Battle Against Limits',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023620/1023620.png',
+            type: 'sstr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta (Giant Ape)',
+            title: 'Furious Last Resort',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4023520/4023520.png',
+            type: 'eteq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Power for the Death Match',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007680/1007680.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Yajirobe',
+            title: 'Wimpy Savior',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026030/1026030.png',
+            type: 'steq',
+            rarity: 'ur'
+        }
+    ],
+]
+
+export default saiyansaga
