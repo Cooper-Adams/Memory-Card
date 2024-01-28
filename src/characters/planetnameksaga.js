909e2173d0ae21e736e573caf369bdcce8dcd0d7
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-28 01:39:37 -0500

    Added character list for characters from the Planet Namek Saga in Dragon Ball Z

---
+++ src/characters/planetnameksaga.js
@@ -0,0 +1,278 @@
+import { nanoid } from 'nanoid'
+
+const nameksaga = [
+    [
+        {
+            id: nanoid(),
+            name: 'Cui',
+            title: 'Imminent Showdown',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001110/1001110.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Dodoria',
+            title: 'Reckless Rage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001050/1001050.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin & Gohan (Kid)',
+            title: 'Adventure on Planet Namek',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022920/1022920.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Advancing Ambition',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010220/1010220.png',
+            type: 'int',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Krillin & Gohan (Kid)',
+            title: 'Swift Attacks in Sync',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022930/1022930.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: "Saiyans' Limitless Potential",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014990/1014990.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Zarbon',
+            title: "Warrior of Beauty's Trump",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001530/1001530.png',
+            type: 'agl',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Appule',
+            title: 'Steadfast Soldier',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1000710/1000710.png',
+            type: 'estr',
+            rarity: 'r'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Captain Ginyu',
+            title: 'First Big Job in Ages',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023760/1023760.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Recoome & Guldo',
+            title: 'Assembled Secret Weapons',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023740/1023740.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Jeice & Burter',
+            title: 'All-Elite Force',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023720/1023720.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)',
+            title: 'Hope for Growth',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1013160/1013160.png',
+            type: 'sstr',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Krillin & Gohan (Kid)',
+            title: 'Dilemma on the Battlefield',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4022950/4022950.png',
+            type: 'sagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Captain Ginyu (Goku)',
+            title: "Captain's Ace in the Hole",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4023780/4023780.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Extraordinary Speed of Growth',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023030/1023030.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Training and Refreshment',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1013210/1013210.png',
+            type: 'sphy',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Nail',
+            title: 'Earnest Determination',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017470/1017470.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Freeza (1st Form)',
+            title: 'Indignant Emperor',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009360/1009360.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin',
+            title: 'Wisdom and Experience',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001680/1001680.png',
+            type: 'agl',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Strategy to Defeat the Emperor',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4022890/4022890.png',
+            type: 'eint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Freeza (2nd Form)',
+            title: 'Storm of Terror',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017160/1017160.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Freeza (3rd Form)',
+            title: 'Eerie Sense of Despair',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022470/1022470.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Battle as a Namekian',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4014690/4014690.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta/Goku',
+            title: 'Saiyan Pride',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023040/1023040.png',
+            type: 'eint',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Freeza (Final Form)',
+            title: 'Horror from Hell',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001580/1001580.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (Kaioken)',
+            title: 'Determined to Fight',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014370/1014370.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Exceptional Wisdom',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022450/1022450.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Saiyan Pride',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4023050/4023050.png',
+            type: 'sint',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Freeza (Final Form)',
+            title: 'Overflowing Bloodlust',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017210/1017210.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Pinnacle of Fury',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4017190/4017190.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Freeza (Full Power)',
+            title: 'Roar of Resentment',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022750/1022750.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Strike of Full Anger',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022630/1022630.png',
+            type: 'sint',
+            rarity: 'lr'
+        }
+    ],
+]
+
+export default nameksaga
