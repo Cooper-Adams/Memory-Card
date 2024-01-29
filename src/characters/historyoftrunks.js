91a03da9904456c33e27a6be32f039bd0a43f67a
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-29 14:18:05 -0500

    Added character list for the Dragon Ball Z special ep History of Trunks

---
+++ src/characters/historyoftrunks.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const historyoftrunks = [
+    [
+        {
+            id: nanoid(),
+            name: 'Androids #17 (Future) & #18 (Future)',
+            title: 'Game of Death',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023360/1023360.png',
+            type: 'eteq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Future) & Trunks (Youth) (Future) / Super Saiyan Gohan (Future)',
+            title: 'Remaining Hope',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023420/1023420.png',
+            type: 'sagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Youth) (Future)',
+            title: 'Roar of Sadness and Rage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/2000220/2000220.png',
+            type: 'int',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen)',
+            title: 'Intersection of Past and Present',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023100/1023100.png',
+            type: 'agl',
+            rarity: 'ssr'
+        }
+    ]
+]
+
+export default historyoftrunks
