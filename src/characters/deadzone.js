d836741def7ec1363857ba3a7e669677f76ee1d8
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-28 21:26:45 -0500

    Added character list for Dragon Ball Z movie Deadzone (and the filler arc)

---
+++ src/characters/deadzone.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const deadzone = [
+    [
+        {
+            id: nanoid(),
+            name: 'Garlic Jr.',
+            title: 'Fight for the Divine Throne',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022050/1022050.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Garlick Jr.',
+            title: 'Demon of Destruction and Vengeance',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022060/1022060.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)',
+            title: 'The Power to Protect',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022220/1022220.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku & Piccolo',
+            title: 'Rivals Acting in Concert',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022120/1022120.png',
+            type: 'sint',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default deadzone
