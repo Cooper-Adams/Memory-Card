e8bd22bebd528b735667f96dda8fe783942d7c0c
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-29 14:32:20 -0500

    Added character list for the Dragon Ball Z movie Cooler's Revenge

---
+++ src/characters/coolersrevenge.js
@@ -0,0 +1,74 @@
+import { nanoid } from 'nanoid'
+
+const coolersrevenge = [
+    [
+        {
+            id: nanoid(),
+            name: 'Cooler',
+            title: 'Conqueror of the Universe',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018180/1018180.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: "Salza (Cooler's Armored Squad)",
+            title: 'Magnificent Squadron',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018060/1018060.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)/Goku',
+            title: 'Courage to Confront Evil',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025110/1025110.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Quick Reactions',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024940/1024940.png',
+            type: 'str',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Cooler',
+            title: 'Planet-Crushing Blow',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024830/1024830.png',
+            type: 'estr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Extraordinary Super Warrior',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024860/1024860.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (Kaioken)',
+            title: 'Last-Second Gambit',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006660/1006660.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Valiant Confrontation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024950/1024950.png',
+            type: 'sstr',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default coolersrevenge
