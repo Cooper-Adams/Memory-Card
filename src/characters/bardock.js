ef5388e32581ba2609e77e0040e5a510c3716491
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-29 14:24:09 -0500

    Added character list for the Dragon Ball Z special ep about Bardock

---
+++ src/characters/bardock.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const bardock = [
+    [
+        {
+            id: nanoid(),
+            name: 'Freeza (1st Form)',
+            title: 'Joy of Destruction',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010040/1010040.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Team Bardock',
+            title: 'Grand Opening',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019100/1019100.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Bardock',
+            title: 'Dazzling Life Force',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014020/1014020.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'King Vegeta',
+            title: 'An Invasion Bet on Primacy',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024030/1024030.png',
+            type: 'eint',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default bardock
