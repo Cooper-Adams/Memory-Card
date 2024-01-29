9caa6d2f5142095d6eb4650aa17f3d631ca0dbeb
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-29 14:16:01 -0500

    Added character list for the Dragon Ball Z movie Lord Slug

---
+++ src/characters/lordslug.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const lordslug = [
+    [
+        {
+            id: nanoid(),
+            name: 'Slug (Giant Form)',
+            title: 'Evil Namekian',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1013710/1013710.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (Kaioken)',
+            title: 'Fusion Power of the Dynamic Duo',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1013730/1013730.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)',
+            title: 'Wings of Wonder',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1013740/1013740.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: "Demon's Intuition",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022300/1022300.png',
+            type: 'str',
+            rarity: 'ssr'
+        }
+    ]
+]
+
+export default lordslug
