2501d1fdf3795c420a4a8c5d400f178e69429372
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-29 15:14:26 -0500

    Added character list for the Dragon Ball Z movie The Return of Cooler

---
+++ src/characters/thereturnofcooler.js
@@ -0,0 +1,74 @@
+import { nanoid } from 'nanoid'
+
+const returnofcooler = [
+    [
+        {
+            id: nanoid(),
+            name: 'Metal Cooler',
+            title: 'Infinite Terror',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024930/1024930.png',
+            type: 'ephy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin & Gohan (Youth)',
+            title: 'Saviors from Earth',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024960/1024960.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku & Super Saiyan Vegeta',
+            title: 'Super Saiyan Duo',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024870/1024870.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Quick-Witted Strategy',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015280/1015280.png',
+            type: 'str',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Metal Cooler Core',
+            title: 'Fusion with the Big Gete Star',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4015370/4015370.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin & Gohan (Youth)',
+            title: 'Strength Concentrated on the Fists',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024970/1024970.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku & Super Saiyan Vegeta',
+            title: 'Full-Power Final Showdown',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4024900/4024900.png',
+            type: 'sagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'A Shot at Victory',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015290/1015290.png',
+            type: 'sstr',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default returnofcooler
