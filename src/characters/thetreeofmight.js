ce4af0cf93c2f1aa83b3219199700ab5c525080d
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-29 14:11:18 -0500

    Added character list for the Dragon Ball Z movie The Tree of Might

---
+++ src/characters/thetreeofmight.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const thetreeofmight = [
+    [
+        {
+            id: nanoid(),
+            name: 'Turles Crusher Corps',
+            title: 'Destroyers of the Universe',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020750/1020750.png',
+            type: 'estr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Power to Defend the Green Planet',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026820/1026820.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)',
+            title: 'Cheerful and Righteous Heart',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025100/1025100.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (Kaioken)',
+            title: 'Transcending Limits',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001400/1001400.png',
+            type: 'agl',
+            rarity: 'ssr'
+        }
+    ]
+]
+
+export default thetreeofmight
