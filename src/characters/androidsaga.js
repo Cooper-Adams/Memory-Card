88c2abf35157f6d3af12c0f40553a5c647be19dd
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-29 13:54:24 -0500

    Added character lists for the Android and Cell sagas of Dragon Ball Z

---
+++ src/characters/androidsaga.js
@@ -0,0 +1,142 @@
+import { nanoid } from 'nanoid'
+
+const androidsaga = [
+    [
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Solitary Warrior',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011400/1011400.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen)',
+            title: 'Intersection of Past and Present',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023100/1023100.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'King Cold',
+            title: 'Mightiest of Monarchs',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/2000190/2000190.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Mecha Freeza',
+            title: 'Explosive Vengeance',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005700/1005700.png',
+            type: 'phy',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Teen)',
+            title: 'Second Super Saiyan',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011720/1011720.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Mecha Freeza & King Cold',
+            title: 'Counterattack of the Wicked Family',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017320/1017320.png',
+            type: 'estr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Miraculous Return',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001750/1001750.png',
+            type: 'phy',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Unwavering Will and Newfound Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017720/1017720.png',
+            type: 'sint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Caught Between Illness and Adversary',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023060/1023060.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'All-Out Intensive Fire',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024000/1024000.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Dr. Gero & Android 19',
+            title: 'Accumulated Bloodlust',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023380/1023380.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta',
+            title: 'Awakened Saiyan Blood',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015030/1015030.png',
+            type: 'eint',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Androids #17 & #18',
+            title: 'Bloodcurdling Activation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018010/1018010.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Teen)',
+            title: 'Future-Carving Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005400/1005400.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta',
+            title: "Gifted Warrior's Exaltation",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006770/1006770.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #18',
+            title: 'Shocking Contact',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017680/1017680.png',
+            type: 'estr',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default androidsaga
