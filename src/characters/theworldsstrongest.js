5fb6e954a37b83e19be9e6baa4adb2c9fb83063b
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-29 14:03:17 -0500

    Added character list for the Dragon Ball Z movie The Worlds Strongest

---
+++ src/characters/theworldsstrongest.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const theworldsstrongest = [
+    [
+        {
+            id: nanoid(),
+            name: 'Dr. Wheelo',
+            title: 'Devilish Genius Scientist',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026730/1026730.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'The Weight of Hope',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003670/1003670.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)',
+            title: 'Exploding Rage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1000870/1000870.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo (Brainwashed)',
+            title: 'Namekian Under Evil Influence',
+            img: 'https://static.wikia.nocookie.net/dbz-dokkanbattle/images/1/1a/Card_1006950_artwork.png/revision/latest?cb=20200520084727',
+            type: 'str',
+            rarity: 'ssr'
+        }
+    ]
+]
+
+export default theworldsstrongest
