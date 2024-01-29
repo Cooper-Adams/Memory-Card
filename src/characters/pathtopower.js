b993624471fbb0c23179ccc15184b5ae377bb837
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-29 13:48:43 -0500

    Added character list for the film Path to Power

---
+++ src/characters/pathtopower.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const pathtopower = [
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (Youth)',
+            title: 'Quest for the Dragon Balls',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025070/1025070.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Yamcha',
+            title: 'Predatory Wolf of the Gale',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025250/1025250.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Hacchan',
+            title: 'All-Out Power of Friendship',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025040/1025040.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Staff Officer Black',
+            title: 'Commander of the Red Ribbon Army',
+            img: 'https://static.wikia.nocookie.net/dbz-dokkanbattle/images/a/ab/Card_1025290_artwork.png/revision/latest?cb=20230103204929',
+            type: 'int',
+            rarity: 'ssr'
+        }
+    ]
+]
+
+export default pathtopower
