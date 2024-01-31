124141baf152ee02541565a08ab00fad187b04a9
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-31 14:57:31 -0500

    Added character list for the Dragon Ball Z filler in the Otherworld

---
+++ src/characters/otherworld.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const otherworld = [
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku (Angel) (Super Kaioken)',
+            title: 'Burning to the Last',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010660/1010660.pngg',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Paikuhan',
+            title: 'Blazing Gale',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025470/1025470.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Perfect Cell (Angel)',
+            title: 'Oppressive Combat',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020600/1020600.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Freeza (Final Form) (Angel)',
+            title: 'Long-Standing Grudge',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020590/1020590.png',
+            type: 'eteq',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default otherworld
