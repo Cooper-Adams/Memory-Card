31cc599cca4b35f23fe1a2c2bdf96ef849962364
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-31 15:03:45 -0500

    Added character list for the Dragon Ball Z saga pertaining to Great Saiyaman

---
+++ src/characters/greatsaiyaman.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const greatsaiyaman = [
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Teen)',
+            title: 'The Spirited Golden Warrior',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009490/1009490.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Great Saiyaman',
+            title: 'Iron Fist of Justice',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009280/1009280.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Videl',
+            title: 'Ally of Love and Friendship',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021810/1021810.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Teen) & Videl',
+            title: 'Brave Pair',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015590/1015590.png',
+            type: 'sint',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default greatsaiyaman
