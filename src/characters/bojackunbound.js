e32850e94bf8ae48065669e597dc0fe5ceb60c61
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-31 14:52:59 -0500

    Added character list for the Dragon Ball Z movie Boujack Unbound

---
+++ src/characters/bojackunbound.js
@@ -0,0 +1,142 @@
+import { nanoid } from 'nanoid'
+
+const bojackunbound = [
+    [
+        {
+            id: nanoid(),
+            name: 'Gohan (Youth)',
+            title: 'Grand Plan',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002680/1002680.png',
+            type: 'phy',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin',
+            title: 'Ready to Fight',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017810/1017810.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen)',
+            title: 'Radiant Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016970/1016970.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Namekian Rage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001630/1001630.png',
+            type: 'phy',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Bido',
+            title: 'Vicious Power Fighter',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/2000730/2000730.png',
+            type: 'phy',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Zangya',
+            title: 'Evil Paralysis',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021030/1021030.png',
+            type: 'eteq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Bujin',
+            title: 'Merciless Supernatural Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/2000740/2000740.png',
+            type: 'int',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Gokua',
+            title: 'Tide-Turning Power-Up',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012350/1012350.png',
+            type: 'estr',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Youth)',
+            title: 'Resolute Valor',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002690/1002690.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Yamcha',
+            title: 'Heightened Intuition',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003520/1003520.png',
+            type: 'int',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Tenshinhan',
+            title: 'Well-Aimed Strike',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026050/1026050.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta',
+            title: 'Willing to Fight',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005370/1005370.png',
+            type: 'agl',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Youth)',
+            title: 'Battle with New Enemies',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021120/1021120.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 2 Gohan (Youth)',
+            title: 'Earth-Protecting Flash',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021140/1021140.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Boujack',
+            title: 'Release of Evil Forces',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027690/1027690.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Full Power Boujack',
+            title: 'Incomparable Opponent',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012150/1012150.png',
+            type: 'eint',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default bojackunbound
