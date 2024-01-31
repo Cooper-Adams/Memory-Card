9fb6c146647bdc85af3db2c8ed424aab8d4d788c
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-31 00:04:57 -0500

    Added character list for the scattered events in Trunks' future timeline before Dragon Ball Super

---
+++ src/characters/trunksfuture.js
@@ -0,0 +1,108 @@
+import { nanoid } from 'nanoid'
+
+const trunksfuture = [
+    [
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen) (Future)',
+            title: 'Return to the Future',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014030/1014030.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Future)',
+            title: 'Alternate Ending',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014050/1014050.png',
+            type: 'sagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #17 (Future)',
+            title: 'A Future Forsaken',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001730/1001730.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #18 (Future)',
+            title: 'Dastardly Demoness',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001740/1001740.png',
+            type: 'int',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Future)',
+            title: 'Victor of the Future',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018150/1018150.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen) (Future)',
+            title: 'In the Name of True Peace',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018140/1018140.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Cell (1st Form)',
+            title: 'The Elusive Beast',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008830/1008830.png',
+            type: 'eteq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Supreme Kai',
+            title: 'Holy Might',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003480/1003480.png',
+            type: 'int',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Babidi & Dabura',
+            title: 'Wicked Wizard and Lord of the Demon Realm',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018590/1018590.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 2 Trunks (Teen)',
+            title: 'Bright Hope',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023390/1023390.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku Black',
+            title: 'Dark Doctrines',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011120/1011120.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Supreme Kai & Kibito',
+            title: 'Noble Creed',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020130/1020130.png',
+            type: 'sint',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default trunksfuture
