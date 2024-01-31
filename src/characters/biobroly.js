5a78b4fad36b29b9b3e474e04207e6a7da46871d
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-31 15:59:03 -0500

    Added character list for the Dragon Ball Z movie Bio-Broly

---
+++ src/characters/biobroly.js
@@ -0,0 +1,74 @@
+import { nanoid } from 'nanoid'
+
+const biobroly = [
+    [
+        {
+            id: nanoid(),
+            name: 'Goten (Kid)',
+            title: 'Fighting Prodigy',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002000/1002000.png',
+            type: 'agl',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Kid)',
+            title: 'Elite Offspring',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001760/1001760.png',
+            type: 'teq',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #18',
+            title: 'Ferocious Counterattack',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1004480/1004480.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Mr. Satan',
+            title: 'Exhilarating Valor',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026320/1026320.png',
+            type: 'phy',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Kid) & Super Saiyan Goten (Kid)',
+            title: 'Pint-Sized Super Warriors',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021690/1021690.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin',
+            title: 'Frontline Courage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021870/1021870.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #18',
+            title: 'Clever Tactics',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008070/1008070.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Bio-Broly',
+            title: 'The Return of The Demon',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005440/1005440.png',
+            type: 'eteq',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default biobroly
