4624fc79de96a3b698b380ae2e9fd3fe20253f00
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-31 14:32:12 -0500

    Added character list for the Dragon Ball Z movie Broly The Legendary Super Saiyan

---
+++ src/characters/brolythelegendarysupersaiyan.js
@@ -0,0 +1,74 @@
+import { nanoid } from 'nanoid'
+
+const lssjbroly = [
+    [
+        {
+            id: nanoid(),
+            name: 'Paragus',
+            title: 'Ambitious Scheme',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/2000760/2000760.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Broly',
+            title: 'Uncontrollable Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021660/1021660.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen)',
+            title: 'Courageous Punch',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021710/1021710.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Never-Ending Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003870/1003870.png',
+            type: 'int',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Legendary Super Saiyan Broly',
+            title: 'Eternal Horror',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007460/1007460.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Eruption of Golden Ki',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022960/1022960.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Teen)',
+            title: 'Counterattack of Light',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021720/1021720.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta',
+            title: 'Reclaim the Summit',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005550/1005550.png',
+            type: 'eteq',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default lssjbroly
