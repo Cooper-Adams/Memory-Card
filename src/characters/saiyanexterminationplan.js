e60907f872c98898af03ebb21c982483ae0ad118
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-31 14:36:43 -0500

    Added character list for the Dragon Ball Z video game movie Saiyan Extermination Plan

---
+++ src/characters/saiyanexterminationplan.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const exterminationplan = [
+    [
+        {
+            id: nanoid(),
+            name: 'Super Hatchiyack',
+            title: "Extermination Plan's Final Weapon",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020430/1020430.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Hatchiyack (Giant Form)',
+            title: 'Limit Breaking Energy of Resentment',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4025650/4025650.png',
+            type: 'eteq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Dr. Lychee',
+            title: 'Vengeful Spirit',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003400/1003400.png',
+            type: 'teq',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Earth-Shaking Immense Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025870/1025870.png',
+            type: 'sstr',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default exterminationplan
