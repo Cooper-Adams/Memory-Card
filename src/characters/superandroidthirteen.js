bb0ade896a6c281d236f20a6fd9493fdf3666f5a
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-31 00:18:46 -0500

    Added character list for the Dragon Ball Z movie Super Android 13

---
+++ src/characters/superandroidthirteen.js
@@ -0,0 +1,74 @@
+import { nanoid } from 'nanoid'
+
+const superandroid13 = [
+    [
+        {
+            id: nanoid(),
+            name: 'Android #13',
+            title: 'Programmed Hatred',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018260/1018260.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Androids #14 & #15',
+            title: 'Revenge Technology',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018520/1018520.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Blazing Warrior',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018470/1018470.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen)',
+            title: 'Magnificent Swordsmanship',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018100/1018100.png',
+            type: 'sphy',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku & Super Saiyan Vegeta & Super Saiyan Trunks (Teen)',
+            title: 'All-Out Ultimate Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028060/1028060.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Miracle-Making Super Saiyan',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018490/1018490.png',
+            type: 'sagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Fusion Android #13',
+            title: 'Light Orb of Hatred',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010790/1010790.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Demonic Pride',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1000900/1000900.png',
+            type: 'int',
+            rarity: 'sr'
+        }
+    ]
+]
+
+export default superandroid13
