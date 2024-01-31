c2f92895cc94a87667677bd241994388a6a9ff81
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-31 15:12:00 -0500

    Added character list for the Dragon Ball Z movie Broly The Second Coming

---
+++ src/characters/brolysecondcoming.js
@@ -0,0 +1,74 @@
+import { nanoid } from 'nanoid'
+
+const brolysecondcoming = [
+    [
+        {
+            id: nanoid(),
+            name: 'Broly',
+            title: 'A Destined Return',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010420/1010420.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goten (Kid)',
+            title: 'Curiosity-Filled Struggle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017530/1017530.png',
+            type: 'sphy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Videl',
+            title: 'Indomitable Breakthrough',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017550/1017550.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Kid)',
+            title: 'Confidence-Oozing Struggle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017540/1017540.png',
+            type: 'sagl',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Teen) & Super Saiyan Goten (Kid)',
+            title: 'Miracle-Calling Clash',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016810/1016810.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Teen)',
+            title: 'Matured Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016890/1016890.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Broly',
+            title: 'Super Saiyan Counterattack',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016860/1016860.png',
+            type: 'eteq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Legendary Super Saiyan Broly',
+            title: 'Nightmarish Impact',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4016880/4016880.png',
+            type: 'eteq',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default brolysecondcoming
