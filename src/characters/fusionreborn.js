f122337a94d1ce0281a9c19298da47a389422007
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-04 00:42:36 -0500

    Added character list for the Dragon Ball Z movie Fusion Reborn

---
+++ src/characters/fusionreborn.js
@@ -0,0 +1,74 @@
+import { nanoid } from 'nanoid'
+
+const fusionreborn = [
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Goku (Angel)',
+            title: 'Burgeoning Strength',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010250/1010250.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Paikuhan',
+            title: "Otherworld Martial Artist's Calling",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021320/1021320.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Janemba',
+            title: 'Otherworld in Collapse',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021290/1021290.png',
+            type: 'eteq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta (Angel)',
+            title: 'Return from the Dark',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005080/1005080.png',
+            type: 'sagl',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Gogeta',
+            title: 'Fused Fighting Force',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4012930/4012930.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Paikuhan',
+            title: 'Righteous Otherworld Defense',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021330/1021330.png',
+            type: 'agl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Janemba',
+            title: 'Boiling Evil',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022590/1022590.png',
+            type: 'ephy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (Angel) & Vegeta (Angel)',
+            title: 'Fusion Reborn',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022190/1022190.png',
+            type: 'steq',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default fusionreborn
