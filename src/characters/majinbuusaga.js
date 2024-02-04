09d885a7a04536dca95f43e1b305ea77d35440c2
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-04 00:26:28 -0500

    Added character list for the Majin Buu Saga of Dragon Ball Z

---
+++ src/characters/majinbuusaga.js
@@ -0,0 +1,482 @@
+import { nanoid } from 'nanoid'
+
+const majinbuusaga = [
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (Angel)',
+            title: 'Message from Another World',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002860/1002860.png',
+            type: 'agl',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Great Saiyaman',
+            title: 'Hero of Love and Justice',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003580/1003580.png',
+            type: 'sagl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goten (Kid)',
+            title: 'Enraged Onslaught',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005090/1005090.png',
+            type: 'teq',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Kid)',
+            title: 'Defying the All-Powerful',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005100/1005100.png',
+            type: 'int',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Videl',
+            title: 'Mesmerizing Might',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001990/1001990.png',
+            type: 'teq',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: "A True Master's Dignity",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005580/1005580.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Supreme Kai',
+            title: "Supreme God's Role",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020120/1020120.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Spopovich',
+            title: 'Savagery Unleashed',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1000610/1000610.png',
+            type: 'teq',
+            rarity: 'r'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Kibito',
+            title: 'Letter of the Law',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003460/1003460.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Great Saiyaman (SS2)',
+            title: 'Furious Instincts',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009480/1009480.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Yamu & Spopovich',
+            title: 'Creeping Conspiracy',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018700/1018700.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Dabura',
+            title: 'Evil Wizardry',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003820/1003820.png',
+            type: 'str',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Dabura',
+            title: "Lord of the Demon Realm's Trial by Fire",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018680/1018680.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Babidi',
+            title: 'Energy to Fulfill Ambitions',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026390/1026390.png',
+            type: 'eint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Exerted Ability',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002460/1002460.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Pui Pui',
+            title: 'Confident Sentry',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002070/1002070.png',
+            type: 'agl',
+            rarity: 'r'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku (Angel)',
+            title: 'Strongest in the Otherworld',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010650/1010650.png',
+            type: 'sstr',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Yakon',
+            title: 'Light-Devouring Beast',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1000630/1000630.png',
+            type: 'agl',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Great Saiyaman',
+            title: 'Full-Throttled Interception',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018570/1018570.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Dabura',
+            title: 'Strike From on High',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010980/1010980.png',
+            type: 'eint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Dabura',
+            title: 'Demonic Lord of Darkness',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001980/1001980.png',
+            type: 'agl',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Good)',
+            title: 'A Monster Unleashed',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001940/1001940.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Great Saiyaman (SS)',
+            title: 'All-Out Resistance',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018580/1018580.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Supreme Kai',
+            title: 'Strength of a God',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012050/1012050.png',
+            type: 'teq',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 2 Goku (Angel)',
+            title: 'Unstoppable Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015870/1015870.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Vegeta',
+            title: 'Fight for Pride',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015880/1015880.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Babidi',
+            title: 'Despicable Mastermind',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1004490/1004490.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Ace Up the Sleeve',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1004700/1004700.png',
+            type: 'teq',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Goku (Angel)',
+            title: 'Astounding Transformation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010260/1010260.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Vegeta',
+            title: 'Beyond the Ferocious Flash',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010160/1010160.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Babidi & Majin Buu (Good)',
+            title: 'Worldwide Terror',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021010/1021010.png',
+            type: 'ephy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gotenks',
+            title: 'Mighty Charge',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015620/1015620.png',
+            type: 'teq',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Evil)',
+            title: 'Astounding Transformation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010830/1010830.png',
+            type: 'eteq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Pure Evil)',
+            title: 'Aggregate of Rage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006730/1006730.png',
+            type: 'ephy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Good)',
+            title: 'Wrathful Eruption',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015110/1015110.png',
+            type: 'estr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gotenks',
+            title: 'Reckless Strength',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011070/1011070.png',
+            type: 'sagl',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Evil)',
+            title: 'Destructive Scattershot Strike',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026430/1026430.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Ultimate Gohan',
+            title: 'Hidden Power Unleashed',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011800/1011800.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Gotenks',
+            title: 'Special Transformation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011050/1011050.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Gotenks & Piccolo',
+            title: 'Unusual Pairing',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025820/1025820.png',
+            type: 'sagl',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Revival for the Entire Universe',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020610/1020610.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Tenshinhan',
+            title: 'Support in the Nick of Time',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020270/1020270.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Gotenks)',
+            title: 'Love of the Throne',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012000/1012000.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Piccolo)',
+            title: 'Concentrated Cells',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020520/1020520.png',
+            type: 'agl',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku & Vegeta (Angel)',
+            title: 'Crossing Convictions',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020450/1020450.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegetto',
+            title: 'The Miracle of Potara',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002390/1002390.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Ultimate Gohan)',
+            title: 'Invincible Absorption',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4020380/4020380.png',
+            type: 'ephy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Vegetto',
+            title: 'Fused Super Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4012900/4012900.png',
+            type: 'sint',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Goku',
+            title: 'Resolve for the Final Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020620/1020620.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta (Angel)',
+            title: 'Support for Rival',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020200/1020200.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Pure)',
+            title: 'Cataclysmic Awakening',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023480/1023480.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Mr.Satan/Majin Buu (Good)',
+            title: "A Friend's Decision",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020770/1020770.png',
+            type: 'sagl',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku & Vegeta',
+            title: "Universe's Last Hope",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4025740/4025740.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 2 Vegeta (Angel)',
+            title: 'A Promise Made to Kakarot',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020220/1020220.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Majin Buu (Pure)',
+            title: 'Majin of Destruction',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023490/1023490.png',
+            type: 'estr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Strike of Gratitude and Respect',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011440/1011440.png',
+            type: 'sagl',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default majinbuusaga
