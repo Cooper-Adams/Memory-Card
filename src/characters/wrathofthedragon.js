1f05ec17c0e7c202cab47da967e2cd68aacc5b71
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-06 10:17:22 -0500

    Added character list for the Dragon Ball Z movie Wrath of the Dragon

---
+++ src/characters/wrathofthedragon.js
@@ -0,0 +1,142 @@
+import { nanoid } from 'nanoid'
+
+const wrathofthedragon = [
+    [
+        {
+            id: nanoid(),
+            name: 'Tapion & Minotia',
+            title: 'True Heroes',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021970/1021970.png',
+            type: 'sstr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Great Saiyaman',
+            title: 'In the Name of Justice',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4019500/4019500.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Great Saiyaman 2/1',
+            title: 'In the Name of Justice',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019490/1019490.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Hirudegarn (Lower Half)',
+            title: 'Half of the Monster',
+            img: 'https://static.wikia.nocookie.net/dbz-dokkanbattle/images/5/5d/Card_1012430_artwork.png/revision/latest/scale-to-width-down/250?cb=20200520095121',
+            type: 'phy',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Kid) & Super Saiyan Goten (Kid)',
+            title: 'Innocent Pair',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021680/1021680.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Tapion (Hirudegarn)',
+            title: "Hero's Secret",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012410/1012410.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Hoi',
+            title: 'Shadowy Sorcerer',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012470/1012470.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Hirudegarn',
+            title: 'An Embodied Nightmare',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028150/1028150.png',
+            type: 'int',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Accrued Strength',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010720/1010720.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Gotenks',
+            title: 'Naught but Rampage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011060/1011060.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Ultimate Gohan',
+            title: 'Exceptional Potential',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006690/1006690.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 2 Vegeta',
+            title: 'Everything at Stake',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009890/1009890.png',
+            type: 'sagl',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Goku',
+            title: 'Mystery Super Technique',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003210/1003210.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Goku',
+            title: 'Golden Fist',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012720/1012720.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Kid)',
+            title: 'Proof of a Hero',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022010/1022010.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Hirudegarn (Complete)',
+            title: 'The Phantom Majin of Fear',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4028170/4028170.png',
+            type: 'eint',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default wrathofthedragon
