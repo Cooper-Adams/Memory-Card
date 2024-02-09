a0f3abcb8fcbbc29934d89dcfdb41f7514a4599d
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-09 16:27:09 -0500

    Added character list for the Dragon Ball Super arc of Universe 6 vs 7

---
+++ src/characters/universe6vs7saga.js
@@ -0,0 +1,142 @@
+import { nanoid } from 'nanoid'
+
+const universe6vs7 = [
+    [
+        {
+            id: nanoid(),
+            name: 'Beerus',
+            title: 'Wayward God',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012680/1012680.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Champa',
+            title: 'Uninhibited Visitor',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016930/1016930.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Vados',
+            title: 'Rules and Restraints',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019830/1019830.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Whis',
+            title: 'Guide to the New Beyond',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005310/1005310.png',
+            type: 'sint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Full Strength',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002430/1002430.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Botamo',
+            title: 'Mystical Body',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007020/1007020.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Heartfelt Transformation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008520/1008520.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Frost (Final Form)',
+            title: 'Evil Nature',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007050/1007050.png',
+            type: 'agl',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Magetta',
+            title: 'Metal Man Strikes',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007080/1007080.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Cabba',
+            title: 'Honest Admiration',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011000/1011000.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Vegeta',
+            title: 'New Realms of Saiyan Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009740/1009740.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Wise Warrior',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011900/1011900.png',
+            type: 'sint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Hit',
+            title: 'Expanding Possibility',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007100/1007100.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Hit',
+            title: 'Growing Through Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011010/1011010.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Goku',
+            title: 'Cyan Juggernaut',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008620/1008620.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Goku',
+            title: 'Next-Level Strike',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008630/1008630.png',
+            type: 'sagl',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default universe6vs7
