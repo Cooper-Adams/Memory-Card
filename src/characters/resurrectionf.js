0b0fa2bc703dfc871dcec7a6bd4c1f829e3e6bd0
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-09 16:07:32 -0500

    Added character list for the Dragon Ball Z movie and Super arc Resurrection F

---
+++ src/characters/resurrectionf.js
@@ -0,0 +1,142 @@
+import { nanoid } from 'nanoid'
+
+const resurrectionf = [
+    [
+        {
+            id: nanoid(),
+            name: 'Beerus',
+            title: 'Sleepiness Caused by Too Much Free Time',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028540/1028540.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Whis',
+            title: 'Godlike Skill',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019350/1019350.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Top-Tier Training',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002130/1002130.png',
+            type: 'teq',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Tireless Training',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002140/1002140.png',
+            type: 'phy',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Tagoma',
+            title: "Underling's Devotion",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002170/1002170.png',
+            type: 'teq',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Sorbet',
+            title: '3rd Stellar Region Officer',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002160/1002160.png',
+            type: 'int',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Shisami',
+            title: 'Crimson Rush',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002590/1002590.png',
+            type: 'str',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Freeza (1st Form)',
+            title: 'First-Ever Training',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021070/1021070.png',
+            type: 'teq',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Master Roshi (Max Power)',
+            title: 'Mighty Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1004830/1004830.png',
+            type: 'int',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Tenshinhan',
+            title: 'Strength of Trust',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002600/1002600.png',
+            type: 'int',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Teen)',
+            title: 'Indomitable Sense of Justice',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002200/1002200.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Divine Wisdom',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002880/1002880.png',
+            type: 'teq',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Freeza (Final Form)',
+            title: 'The Nightmare Returns',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002150/1002150.png',
+            type: 'agl',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Golden Freeza',
+            title: "Emperor's Newfound Power",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024510/1024510.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Goku',
+            title: 'The Paramount Saiyan',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008610/1008610.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Vegeta',
+            title: 'Divine Combat Begins',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4024450/4024450.png',
+            type: 'sint',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default resurrectionf
