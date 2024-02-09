b89adc09bdca3e7c51c9d236b172e86f9f5be863
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-09 14:04:03 -0500

    Added character list for the Dragon Ball Z Movie and Super arc Battle of Gods

---
+++ src/characters/battleofgods.js
@@ -0,0 +1,142 @@
+import { nanoid } from 'nanoid'
+
+const battleofgods = [
+    [
+        {
+            id: nanoid(),
+            name: 'Beerus',
+            title: 'Awakened God of Destruction',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1013750/1013750.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Whis',
+            title: 'Mysterious Mentor',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002670/1002670.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Goku',
+            title: 'The Power to Shake the Universe',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001920/1001920.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'King Kai',
+            title: 'King of the North Worlds',
+            img: 'https://static.wikia.nocookie.net/dbz-dokkanbattle/images/9/9e/Card_1007580_artwork.png/revision/latest/scale-to-width-down/250?cb=20160828144926',
+            type: 'phy',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gotenks',
+            title: 'True Child Prodigy',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007200/1007200.png',
+            type: 'steq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Mr. Buu',
+            title: 'Irrepressible Impact',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005760/1005760.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta',
+            title: 'Assault with Pride Put Aside',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026270/1026270.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Beerus',
+            title: "God of Destruction's Ultimatum",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012670/1012670.png',
+            type: 'int',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Beerus',
+            title: 'Umpire of Annihilation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1004880/1004880.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Furious Vegeta',
+            title: 'Outburst of Emotions',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4012650/4012650.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Quiet Grit',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007830/1007830.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Defying God',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018240/1018240.png',
+            type: 'sint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God Goku',
+            title: 'Divine Fighter Born of Light',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026240/1026240.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God Goku',
+            title: 'Legend Reborn',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018250/1018250.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Saiyan of a New Dimension',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019260/1019260.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Beerus & Whis',
+            title: 'Global Harmony Through Destruction',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1013770/1013770.png',
+            type: 'sstr',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default battleofgods
