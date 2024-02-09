cc0c74720be8cc53d6c85fa87a9acc1911844b0c
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-09 16:33:16 -0500

    Added character list for the Dragon Ball Super Copy Vegeta arc

---
+++ src/characters/copyvegeta.js
@@ -0,0 +1,40 @@
+import { nanoid } from 'nanoid'
+
+const copyvegeta = [
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Gotenks',
+            title: 'Power that Outshines the Adults',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016210/1016210.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Duplicate Vegeta',
+            title: 'The Threat of the Superhuman Water',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016190/1016190.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Duplicate Vegeta (Super Saiyan God SS)',
+            title: 'Duplicated Ability',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016200/1016200.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Goku',
+            title: 'Indomitable Battle Lust',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009530/1009530.png',
+            type: 'str',
+            rarity: 'ssr'
+        }
+    ]
+]
+
+export default copyvegeta
