669523ce590c1b7ee1daf7122225f71f3cb56733
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-12 14:42:30 -0500

    Added Character list for the Dragon Ball Super Future Saga

---
+++ src/characters/futuresaga.js
@@ -0,0 +1,312 @@
+import { nanoid } from 'nanoid'
+
+const futuresaga = [
+    [
+        {
+            id: nanoid(),
+            name: 'Goku Black',
+            title: 'A Future Destroyed',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008600/1008600.png',
+            type: 'teq',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Future)',
+            title: "Humanity's Hope",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008870/1008870.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Mai (Future)',
+            title: 'Fighting Eradication',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008880/1008880.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen) (Future)',
+            title: 'Confronting the Darkness',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008860/1008860.png',
+            type: 'teq',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen) (Future)',
+            title: 'New Resolution for the Future',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017070/1017070.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku Black',
+            title: 'Warning From the Future',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008600/1008600.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Zamasu',
+            title: 'Doubts about Mortals and Gods',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027380/1027380.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 2 Goku',
+            title: 'Shattering Strike',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008580/1008580.png',
+            type: 'sphy',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen) (Future)',
+            title: 'The Duty of Humanity',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015610/1015610.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Goku',
+            title: 'Drive to Win',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012330/1012330.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Vegeta',
+            title: 'Saiyan Tenacity',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009540/1009540.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku Black (Super Saiyan Rosé)',
+            title: 'Exalted Ideals',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009340/1009340.png',
+            type: 'agl',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Gowasu & Zamasu',
+            title: 'A Lesson in Good and Evil',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017020/1017020.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Zamasu',
+            title: 'Rebellion Against Divine Justice',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027390/1027390.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Beerus',
+            title: 'Devastating Punishment',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005300/1005300.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Zamasu (Goku)',
+            title: 'Dawn of Darkest Justice',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019070/1019070.png',
+            type: 'estr',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku Black & Zamasu',
+            title: 'Distorted Justice',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011130/1011130.png',
+            type: 'eteq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku Black (Super Saiyan Rosé)',
+            title: 'Rose Stained Super Saiyan',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009350/1009350.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Vegeta',
+            title: 'Uncontrollable Rage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009750/1009750.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Future)',
+            title: 'Power of Rage',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009560/1009560.png',
+            type: 'sstr',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Future)',
+            title: 'Unwavering Conviction',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012960/1012960.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Future) & Mai (Future)',
+            title: 'Human Pride',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019280/1019280.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Vegeta',
+            title: 'Passion of the Warrior Race',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009590/1009590.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku Black (Super Saiyan Rosé)',
+            title: 'Furious Punishment',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1012760/1012760.png',
+            type: 'estr',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Fusion Zamasu',
+            title: 'Dawn of an Ideal World',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024660/1024660.png',
+            type: 'eint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Goku & Super Saiyan God SS Vegeta',
+            title: 'All-Out Final Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027120/1027120.png',
+            type: 'sagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Fusion Zamasu',
+            title: 'Contradictions of Gods and Mortals',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027200/1027200.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Vegeta & Super Saiyan Trunks (Future)',
+            title: 'Power of Pride and Hope',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024550/1024550.png',
+            type: 'sint',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Vegetto',
+            title: 'Ultimate and Invincible Fusion',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018650/1018650.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Fusion Zamasu',
+            title: 'Wrath of the Absolute God',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009790/1009790.png',
+            type: 'eteq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Vegetto',
+            title: 'All or Nothing',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011680/1011680.png',
+            type: 'sstr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Future)',
+            title: 'Resilient Will to Protect the Future',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4017090/4017090.png',
+            type: 'steq',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Future)',
+            title: 'Hope at the End of a Deadly Showdown',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027290/1027290.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (Future)',
+            title: 'Hope at the End of a Deadly Showdown',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4027300/4027300.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Fusion Zamasu',
+            title: 'Infinite Sanctuary',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027220/1027220.png',
+            type: 'eteq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Infinite Zamasu',
+            title: 'Infinite Sanctuary',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4027230/4027230.png',
+            type: 'eteq',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default futuresaga
