9d41ba5cf566e88eddeaea1589a9b7d1670d8123
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-14 13:35:09 -0500

    Added character list for the movie Dragon Ball Super: Broly

---
+++ src/characters/superbroly.js
@@ -0,0 +1,210 @@
+import { nanoid } from 'nanoid'
+
+const superbroly = [
+    [
+        {
+            id: nanoid(),
+            name: 'Freeza (1st Form)',
+            title: 'Immediate Restraint',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028600/1028600.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Bardock & Gine',
+            title: "Parents' Guidance",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019040/1019040.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Training to Combat Freeza',
+            img: 'https://static.wikia.nocookie.net/dbz-dokkanbattle/images/2/21/Card_1019110_artwork.png/revision/latest/scale-to-width-down/250?cb=20200519082449',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Training to Become the Strongest',
+            img: 'https://static.wikia.nocookie.net/dbz-dokkanbattle/images/a/a5/Card_1019120_artwork.png/revision/latest/scale-to-width-down/250?cb=20200519082212',
+            type: 'str',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Prince of the Lost Planet',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016110/1016110.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Low-Class Warrior Exiled to Earth',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016090/1016090.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Broly',
+            title: 'Saiyan Outcast',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014530/1014530.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Freeza (Final Form)',
+            title: 'Tyrannical Despot',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016140/1016140.png',
+            type: 'phy',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta',
+            title: 'Rapid Clash',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016040/1016040.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Intensifying Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016020/1016020.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Paragus & Broly',
+            title: 'Troublesome Father and Son',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016080/1016080.png',
+            type: 'eint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Broly',
+            title: 'Warrior in Exile',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016060/1016060.png',
+            type: 'agl',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God Vegeta',
+            title: 'Devastating Fighting Spirit of God',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016050/1016050.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God Goku',
+            title: "God's Effortless Controlling Aura",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016030/1016030.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Broly',
+            title: 'Wrathful Saiyan',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015990/1015990.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Broly (Wrathful)',
+            title: 'Unusual Evolution',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014510/1014510.png',
+            type: 'eagl',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Goku & Super Saiyan God SS Vegeta',
+            title: 'Divine Warriors with Infinite Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4022390/4022390.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gogeta',
+            title: "Ultimate and Supreme Fusion",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018620/1018620.png',
+            type: 'sstr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Broly',
+            title: 'Fierce Warrior',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028010/1028010.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Golden Freeza',
+            title: 'Proof of Rulership',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028220/1028220.png',
+            type: 'int',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Broly (Full Power)',
+            title: 'The Great Explosion of Heightened Fighting Spirit',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4028030/4028030.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gogeta',
+            title: 'Transcendent Fusion',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1018620/1018620.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Broly',
+            title: 'Endless Evolution of the Warrior Race',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016000/1016000.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan God SS Gogeta',
+            title: 'Diffuse Reflection of Overflowing Fighting Spirit',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4027990/4027990.png',
+            type: 'sphy',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default superbroly
