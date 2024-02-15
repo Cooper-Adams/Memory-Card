d29049de871d81b742cb19d91f2e26c7bad27a5b
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-15 00:36:09 -0500

    Added the file that will compile the full list of characters

---
+++ src/characters/_fulllist.js
@@ -0,0 +1,69 @@
+import dbsaga from './dragonballsaga'
+import pathtopower from './pathtopower'
+import deadzone from './deadzone'
+import saiyansaga from './saiyansaga'
+import theworldsstrongest from './theworldsstrongest'
+import thetreeofmight from './thetreeofmight'
+import lordslug from './lordslug'
+import planetnameksaga from './planetnameksaga'
+import bardock from './bardock'
+import coolersrevenge from './coolersrevenge'
+import returnofcooler from './thereturnofcooler'
+import androidsaga from './androidsaga'
+import superandroid13 from './superandroidthirteen'
+import historyoftrunks from './historyoftrunks'
+import lssjbroly from './brolythelegendarysupersaiyan'
+import cellsaga from './cellsaga'
+import trunksfuture from './trunksfuture'
+import exterminationplan from './saiyanexterminationplan'
+import bojackunbound from './bojackunbound'
+import otherworld from './otherworld'
+import greatsaiyaman from './greatsaiyaman'
+import brolysecondcoming from './brolysecondcoming'
+import biobroly from './biobroly'
+import majinbuusaga from './majinbuusaga'
+import fusionreborn from './fusionreborn'
+import wrathofthedragon from './wrathofthedragon'
+import battleofgods from './battleofgods'
+import resurrectionf from './resurrectionf'
+import universe6vs7 from './universe6vs7saga'
+import copyvegeta from './copyvegeta'
+import futuresaga from './futuresaga'
+import tournamentofpower from './tournamentofpower'
+import superbroly from './superbroly'
+
+const fullList = dbsaga.concat(pathtopower, 
+    deadzone, 
+    saiyansaga, 
+    theworldsstrongest, 
+    thetreeofmight, 
+    lordslug, 
+    planetnameksaga, 
+    bardock, 
+    coolersrevenge, 
+    returnofcooler, 
+    androidsaga, 
+    superandroid13, 
+    historyoftrunks,
+    lssjbroly,
+    cellsaga,
+    trunksfuture,
+    exterminationplan,
+    bojackunbound,
+    otherworld,
+    greatsaiyaman,
+    brolysecondcoming,
+    biobroly,
+    majinbuusaga,
+    fusionreborn,
+    wrathofthedragon,
+    battleofgods,
+    resurrectionf,
+    universe6vs7,
+    copyvegeta,
+    futuresaga,
+    tournamentofpower,
+    superbroly
+)
+
+export default fullList
