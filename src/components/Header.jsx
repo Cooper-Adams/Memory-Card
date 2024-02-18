085dc69ae9cbfc5e4aa70b7c372984d1e7616d4a
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-18 18:43:10 -0500

    Added the current saga name to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = (props) => {
     return (
         <header className='header'>
             <h2 className='h-title'>DOKKAN Memory Cards</h2>
+            <h2 className='h-saga-title'>{props.sagaName}</h2>
             <div className='h-score-container'>
                 <h2 className='h-score'>SCORE: {props.score}</h2>
                 <h2 className='h-highscore'>HIGH SCORE: {props.highScore}</h2>
