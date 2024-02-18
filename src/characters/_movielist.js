6bedd2918e5b17576218e5d79b1af3b1e67ab5cf
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-18 18:36:10 -0500

    Added the name list for the Dragon Ball Z movies

---
+++ src/characters/_movielist.js
@@ -0,0 +1,22 @@
+const movieList = [
+    'Dragon Ball: The Path to Power',
+    'Dragon Ball Z the Movie: Dead Zone',
+    "Dragon Ball Z the Movie: The World's Strongest",
+    'Dragon Ball Z: The Decisive Battle for the Whole Earth',
+    'Dragon Ball Z: Super Saiyan Son Goku',
+    'Dragon Ball Z: The Outstanding Strongest vs. Strongest',
+    'Dragon Ball Z: Clash!! 10 Billion Power Warriors',
+    'Dragon Ball Z: Extreme Battle!! Three Great Super Saiyans',
+    'Dragon Ball Z: Burn!! Hot Battle - Fierce Battle - Super Fierce Battle',
+    'Dragon Ball Z: The Last Minute of the Galaxy!! A Super Incredible Guy',
+    "Dragon Ball Z: Two Dangerous People! Super Warriors Can't Sleep",
+    "Dragon Ball Z Defeat the Super Warrior!! I'll Be The Winner",
+    'Dragon Ball Z: Resurrection Fusion!! Goku and Vegeta',
+    "Dragon Ball Z: Dragon Fist Explosion!! Who Will Do It If Goku Doesn't Do It?",
+    'Dragon Ball Z: Battle of Gods',
+    "Dragon Ball Z: Resurrection 'F'",
+    'Dragon Ball Super: Broly',
+    'Dragon Ball Super: Super Hero'
+]
+
+export default movieList
