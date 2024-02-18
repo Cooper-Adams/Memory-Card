7797533bef01b328979ed393129125008b28f71d
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-18 18:35:44 -0500

    Added the name list for the sagas of Dragon Ball Z

---
+++ src/characters/_dragonballz.js
@@ -0,0 +1,14 @@
+const zList = [
+    'Saiyan Saga',
+    'Planet Namek Saga',
+    'Dragon Ball Z: A Lonesome, Final Battle - The Father of Z Warrior Kakarot, who Challenged Freeza',
+    'Androids Saga',
+    'Cell Saga',
+    'Trunks of the Future Timeline',
+    'Other World Saga',
+    'Great Saiyaman Saga',
+    'Majin Buu Saga',
+    'Peaceful World Saga',
+]
+
+export default zList
