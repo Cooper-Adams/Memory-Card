c1ab0593c978fcdf5c222481473c4d975c13587a
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-18 18:35:12 -0500

    Added the name list for the sagas of Dragon Ball Super

---
+++ src/characters/_dragonballsuper.js
@@ -0,0 +1,12 @@
+const superList = [
+    'Dragon Ball Z: Battle of Gods',
+    "Dragon Ball Z: Resurrection 'F'",
+    'Universe 6 Saga',
+    'Copy-Vegeta Saga',
+    'Future Saga',
+    'Universe Survival Saga',
+    'Dragon Ball Super: Broly',
+    'Dragon Ball Super: Super Hero'
+]
+
+export default superList
