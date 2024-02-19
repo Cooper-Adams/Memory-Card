c13e3a6435e89358e244e03aa9d4f7156ef5b7ad
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-19 17:40:26 -0500

    Added a title to each of the mode selection images

--- src/components/ChooseMode.jsx
+++ src/components/ChooseMode.jsx
@@ -10,18 +10,22 @@ const ChooseMode = (props) => {
             <div className='mode-box'>
                 <div className='modal-box'>
                     <img onClick={gameMode} title='full' className='modal-img' src='src/assets/Full.jpg' alt='All Dragon Ball Sagas'/>
+                    <h1 className='saga-title'>All Sagas</h1>
                 </div>
 
                 <div className='modal-box'>
                     <img onClick={gameMode} className='modal-img' title='z' src='src/assets/Z.jpg' alt='Sagas from Dragon Ball Z'/>
+                    <h1 className='saga-title'>Dragon Ball Z Sagas</h1>
                 </div>
 
                 <div className='modal-box'>
                     <img onClick={gameMode} className='modal-img' title='super' src='src/assets/Super.jpg' alt='Sagas from Dragon Ball Super'/>
+                    <h1 className='saga-title'>Dragon Ball Super Sagas</h1>
                 </div>
 
                 <div className='modal-box'>
                     <img onClick={gameMode} className='modal-img' title='movies' src='src/assets/Movies.png' alt='Sagas from Dragon Ball Z Movies'/>
+                    <h1 className='saga-title'>Dragon Ball Movie Sagas</h1>
                 </div>
             </div>
         </div>
