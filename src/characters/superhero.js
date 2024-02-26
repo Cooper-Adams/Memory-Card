76aa16fe9c004b6292f58bda7768a2841b9fbe3c
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-26 12:52:49 -0500

    Added character list for the movie Dragon Ball Super: Super Hero

---
+++ src/characters/superhero.js
@@ -0,0 +1,210 @@
+import { nanoid } from 'nanoid'
+
+const superhero = [
+    [
+        {
+            id: nanoid(),
+            name: 'Pan (Kid)',
+            title: 'Promising New Blood',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026090/1026090.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: 'Burst of Fighting Spirit',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009220/1009220.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Gamma 2',
+            title: 'Fearless Hero',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025530/1025530.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Gamma 1',
+            title: 'Red-Caped Android',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026170/1026170.png',
+            type: 'teq',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Vegeta',
+            title: 'Exerted Ability',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002460/1002460.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Fighting Spirit to Overcome Adversity',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026150/1026150.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Broly & Cheelai & Lemo',
+            title: 'A New Life on Vampa',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019000/1019000.png',
+            type: 'ephy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Whis',
+            title: 'Angelic Instruction',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1019660/1019660.png',
+            type: 'sagl',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Gohan (Teen)',
+            title: 'Comeback and Rescue',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024560/1024560.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gamma 1',
+            title: 'Birth of a New Android',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025500/1025500.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gamma 2',
+            title: 'Attack of a New Android',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025520/1025520.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo (Power Awakening)',
+            title: 'New Form and Awakened Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025570/1025570.png',
+            type: 'teq',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Teen)',
+            title: 'Full Power Transformation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025600/1025600.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Ultimate Gohan',
+            title: 'Clashing Justice',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025550/1025550.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Gamma 1 & Gamma 2/Gamma 1',
+            title: 'Brilliant Shining Heroes',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028370/1028370.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo (Power Awakening)',
+            title: 'Exploding Latent Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025590/1025590.png',
+            type: 'steq',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen) & Goten (Teen)',
+            title: 'Desire for New Fights',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026070/1026070.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Krillin & Android #18',
+            title: 'Friends Who Overcome Difficulties',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028470/1028470.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Gamma 1 & Gamma 2/Gamma 1',
+            title: 'Brilliant Shining Heroes',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4028380/4028380.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Cell Max',
+            title: 'Re-Engineered Monstrosity',
+            img: 'src/assets/cellmax.png',
+            type: 'str',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Piccolo (Power Awakening)',
+            title: 'Exploding Latent Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025590/1025590.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Cell Max',
+            title: 'Re-Engineered Monstrosity',
+            img: 'src/assets/cellmax.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Ultimate Gohan',
+            title: 'Awakened Super Hero',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025560/1025560.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Beast)',
+            title: 'New Power Beyond Ultimate',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1028320/1028320.png',
+            type: 'sstr',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default superhero
