e73d06d8281f43b93684c52d0151b62821cfd365
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-02-29 01:53:00 -0500

    Added a number of sound files along with a list to give them key value pairs

---
+++ src/assets/sounds/_musicList.js
@@ -0,0 +1,75 @@
+import titleScreen from './7thAnniHomeScreen.mp3'
+import dbsaga from './INTPiccolo.webm'
+import pathtopower from './PHYKidGoku.webm'
+import deadzone from './LRGokuPiccolo.webm'
+import saiyansaga from './LRKidGohan.webm'
+import theworldsstrongest from './LRWheelo.webm'
+import thetreeofmight from './LRIntGoku.webm'
+import lordslug from './LRStrKaiokenGoku.webm'
+import planetnameksaga from './LRIntSSJGoku.webm'
+import bardock from './TEQBardock.webm'
+import coolersrevenge from './LRTeqGoku.webm'
+import returnofcooler from './LRAglGokuVegeta.webm'
+import androidsaga from './LRPhyAndroids.webm'
+import superandroid13 from './LRIntSSJTrio.webm'
+import historyoftrunks from './LRFuture.webm'
+import lssjbroly from './INTBroly.webm'
+import cellsaga from './STRGokuGohan.webm'
+import trunksfuture from './INTTrunks.webm'
+import exterminationplan from './LRStrGoku.webm'
+import bojackunbound from './LRTeqSSJ2Gohan.webm'
+import otherworld from './INTSSJ3Goku.webm'
+import greatsaiyaman from './TEQUltimateGohan.webm'
+import brolysecondcoming from './LRGohanGoten.webm'
+import biobroly from './PHYGotenTrunks.webm'
+import majinbuusaga from './LRPhyGokuVegeta.webm'
+import fusionreborn from './LRTeqGogeta.webm'
+import wrathofthedragon from './STRSSJ3Goku.webm'
+import battleofgods from './PHYGodGoku.webm'
+import resurrectionf from './LRAglGoldenFreeza.webm'
+import universe6vs7 from './LRStrGokuHit.webm'
+import copyvegeta from './LRAglGokuVegetaBlue.webm'
+import futuresaga from './LRTeqVegito.webm'
+import tournamentofpower from './INTUiGoku.webm'
+import superbroly from './LRPhyGogeta.webm'
+import superhero from './LRBeastGohan.webm'
+
+let musicList = {}
+
+musicList['Home'] = titleScreen
+musicList['Dragon Ball'] = dbsaga
+musicList['Dragon Ball: The Path to Power'] = pathtopower
+musicList['Dragon Ball Z: The Movie - Dead Zone'] = deadzone
+musicList['Saiyan Saga'] = saiyansaga
+musicList["Dragon Ball Z: The Movie - The World's Strongest"] = theworldsstrongest
+musicList['Dragon Ball Z: The Decisive Battle for the Whole Earth'] = thetreeofmight
+musicList['Dragon Ball Z: Super Saiyan Son Goku'] = lordslug
+musicList['Planet Namek Saga'] = planetnameksaga
+musicList['Dragon Ball Z: A Lonesome, Final Battle - The Father of Z Warrior Kakarot, who Challenged Freeza'] = bardock
+musicList['Dragon Ball Z: The Outstanding Strongest vs. Strongest'] = coolersrevenge
+musicList['Dragon Ball Z: Clash!! 10 Billion Power Warriors'] = returnofcooler
+musicList['Androids Saga'] = androidsaga
+musicList['Dragon Ball Z: Extreme Battle!! Three Great Super Saiyans'] = superandroid13
+musicList['Dragon Ball Z: Defiance in the Face of Despair!! The Remaining Super-Warriors: Gohan and Trunks'] = historyoftrunks
+musicList['Dragon Ball Z: Burn!! Hot Battle - Fierce Battle - Super Fierce Battle'] = lssjbroly
+musicList['Cell Saga'] = cellsaga
+musicList['Trunks of the Future Timeline'] = trunksfuture
+musicList['Dragon Ball Z Gaiden: Saiya-jin Zetsumetsu Keikaku'] = exterminationplan
+musicList['Dragon Ball Z: The Last Minute of the Galaxy!! A Super Incredible Guy'] = bojackunbound
+musicList['Other World Saga'] = otherworld
+musicList['Great Saiyaman Saga'] = greatsaiyaman
+musicList["Dragon Ball Z: Two Dangerous People! Super Warriors Can't Sleep"] = brolysecondcoming
+musicList["Dragon Ball Z Defeat the Super Warrior!! I'll Be The Winner"] = biobroly
+musicList['Majin Buu Saga'] = majinbuusaga
+musicList['Dragon Ball Z: Resurrection Fusion!! Goku and Vegeta'] = fusionreborn
+musicList["Dragon Ball Z: Dragon Fist Explosion!! Who Will Do It If Goku Doesn't Do It?"] = wrathofthedragon
+musicList['Dragon Ball Z: Battle of Gods'] = battleofgods
+musicList["Dragon Ball Z: Resurrection 'F'"] = resurrectionf
+musicList['Universe 6 Saga'] = universe6vs7
+musicList['Copy-Vegeta Saga'] = copyvegeta
+musicList['Future Saga'] = futuresaga
+musicList['Universe Survival Saga'] = tournamentofpower
+musicList['Dragon Ball Super: Broly'] = superbroly
+musicList['Dragon Ball Super: Super Hero'] = superhero
+
+export default musicList
