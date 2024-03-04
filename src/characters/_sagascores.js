722348b6e117eb0c854f8f2911c19825e8ab1a19
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-03 20:13:33 -0500

    Added an array of maximum scores for each mode

---
+++ src/characters/_sagascores.js
@@ -0,0 +1,8 @@
+let scoreList = {}
+
+scoreList['full'] = 548 //TEMP UNTIL ALL SAGAS FINISHED
+scoreList['dbz'] = 180 //TEMP UNTIL PEACEFUL WORLD IS ADDED
+scoreList['dbs'] = 216
+scoreList['movies'] = 188
+
+export default scoreList
