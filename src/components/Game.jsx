98d57ce9f765a9d6d4e4e10a7bcc9af65454444d
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-04 21:11:01 -0500

    Added the lodash shuffle as I was extremely frustrated with the broken shuffling from before

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,52 +1,21 @@
 import Card from './Card'
 import fullList from '../characters/_fulllist'
 import React, {useEffect, useState} from 'react'
-import movieList from '../characters/_movielist'
-import sagaList from '../characters/_sagalist'
-import superList from '../characters/_dragonballsuper'
-import zList from '../characters/_dragonballz'
+import shuffle from 'lodash.shuffle'
 
 let charList = []
 
 const Game = (props) => {
     const [clickedCards, setClickedCards] = useState([])
-    const [level, setLevel] = useState(0)
     const [stage, setStage] = useState(0)
 
-    /* Grabs the next array of characters */
-    if (props.mode == 'full') {
-        useEffect(() => {
-            props.setSagaName(sagaList[level])
-        })
-        charList = fullList[sagaList[level]]
-    } else if (props.mode == 'z') { 
-        useEffect(() => {
-            props.setSagaName(zList[level])
-        })
-        charList = fullList[zList[level]]
-    } else if (props.mode == 'super') {
-        useEffect(() => {
-            props.setSagaName(superList[level])
-        })
-        charList = fullList[superList[level]] 
-    } else if (props.mode == 'movies') {
-        useEffect(() => {
-            props.setSagaName(movieList[level])
-        })
-        charList = fullList[movieList[level]]
-    }
+    charList = fullList[props.sagaName]
 
     /* Updates the record of cards that have been clicked */
     const updateClickedCards = (card) => { setClickedCards([...clickedCards, card]) }
 
     /* Erases the record of cards that have been clicked */
     const eraseClickedCards = () => { setClickedCards([]) }
-
-    /* Shuffles the card map with the Fisher-Yates algo */
-    // for (let i = (charList[stage].length - 1); i > 0; i--) {
-    //     let j = Math.floor(Math.random() * (i + 1))
-    //     charList[stage][i] = charList[stage].splice(j, 1, charList[stage][i])[0]
-    // }
 
     /* Grabs the info for each character in the level */
     let charMap = charList[stage].map((card) => {
@@ -59,8 +28,6 @@ const Game = (props) => {
                 img={card.img}
                 type={card.type}
                 rarity={card.rarity}
-                clickedCards = {clickedCards}
-                eraseClickedCards={eraseClickedCards}
                 updateClickedCards={updateClickedCards}
                 updateScore={props.updateScore}
                 setGameOver={props.setGameOver}
@@ -70,16 +37,19 @@ const Game = (props) => {
 
     /* Use effect here to change the level when the score for that level has been maxed */
     useEffect(() => {
-        if (clickedCards.length == 4) {
+        if (clickedCards.length >= 4) {
             eraseClickedCards()
             if (charList.length == (stage + 1)) {
                 setStage(0)
-                setLevel(level + 1)
+                props.setLevel(props.level + 1)
             } else {
                 setStage(stage + 1)
             }
         }
     }, [clickedCards])
+
+    /* lodash shuffle the stage */
+    charList[stage] = shuffle(charList[stage])
 
     return (
         <div className='game-div'>
