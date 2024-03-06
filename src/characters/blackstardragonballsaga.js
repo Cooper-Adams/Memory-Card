78843f5b3b9a6fa81bf6464f2e21e791b9b28cc8
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-06 17:47:24 -0500

    Added character list for Dragon Ball GT's Black Star Dragon Ball Saga

---
+++ src/characters/blackstardragonballsaga.js
@@ -0,0 +1,108 @@
+import { nanoid } from 'nanoid'
+
+const blackstardragonballs = [
+    [
+        {
+            id: nanoid(),
+            name: 'Uub',
+            title: 'Soul Succession',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005250/1005250.png',
+            type: 'teq',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: '5 Years of Training',
+            img: 'src/assets/gtgoku.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: "Explorer's Premonition",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008380/1008380.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Pan (GT)',
+            title: 'Impassioned Strike',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005870/1005870.png',
+            type: 'teq',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: 'Going Galactic',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002960/1002960.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Pan (GT)',
+            title: 'Curiosity for the Unknown',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002980/1002980.png',
+            type: 'int',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (GT)',
+            title: 'Exceptional Strength',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022660/1022660.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Ledgic',
+            title: 'Formidable Foe',
+            img: 'src/assets/ledgic.png',
+            type: 'agl',
+            rarity: 'r'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Luud',
+            title: "Dr Myuu's Machine Mutant",
+            img: 'src/assets/Luud.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Full Power Luud',
+            title: "Dr Myuu's Destroyer",
+            img: 'src/assets/FPLuud.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (GT) & Pan (GT) & Trunks (GT)',
+            title: 'Endless Adventure',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016370/1016370.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Para Para Brothers',
+            title: 'Suspicious Undercover Activity',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024100/1024100.png',
+            type: 'phy',
+            rarity: 'ssr'
+        }
+    ]
+]
+
+export default blackstardragonballs
