21692ffb4b08636911a0ec857514582bb1b0d36e
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-06 13:17:55 -0500

    Added character list for the end of Dragon Ball Z

---
+++ src/characters/peacefulworld.js
@@ -0,0 +1,74 @@
+import { nanoid } from 'nanoid'
+
+const peacefulworld = [
+    [
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'Reaching New Heights',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003140/1003140.png',
+            type: 'int',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goten (Teen)',
+            title: 'Easy Test of Strength',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024200/1024200.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Teen)',
+            title: 'At Ease When Back on the Front Line',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024220/1024220.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Uub (Youth)',
+            title: 'Diligent Firstborn',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020800/1020800.png',
+            type: 'phy',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Uub (Youth)',
+            title: 'Astonishing Reincarnation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020810/1020810.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku',
+            title: 'A Dream Come True',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016330/1016330.png',
+            type: 'sint',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Pan (Kid)',
+            title: 'Promising Girl',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024160/1024160.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Uub (Youth)',
+            title: 'Bright Future',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003700/1003700.png',
+            type: 'agl',
+            rarity: 'sr'
+        }
+    ]
+]
+
+export default peacefulworld
