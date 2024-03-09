327eba70a0ffc069394d1a63a4ded8e9c8b50cb4
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-09 15:16:51 -0500

    Added high scores for separate game modes and finished the reset functions

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,15 @@ player.addEventListener('canplay', () => {
 
 const App = (props) => {
     const [gameOver, setGameOver] = useState(false)
-    const [highScore, setHighScore] = useState(0)
+    const [allHS, setAllHS] = useState(0)
+    const [dbzHS, setDBZHS] = useState(0)
+    const [dbsHS, setDBSHS] = useState(0)
+    const [dbmHS, setDBMHS] = useState(0)
+    const [dbgtHS, setDBGTHS] = useState(0)
     const [isPlaying, setIsPlaying] = useState(true)
     const [level, setLevel] = useState(0)
     const [mode, setMode] = useState('')
+    const [modeScore, setModeScore] = useState(0)
     const [sagaName, setSagaName] = useState('Home')
     const [score, setScore] = useState(0)
 
@@ -39,6 +44,16 @@ const App = (props) => {
 
     /* Resets the game, but not the high score */
     const resetGame = () => {
+        setLevel(0)
+        setMode('')
+        setSagaName('Home')
+        setScore(0)
+        setGameOver(false)
+    }
+
+    /* Resets the current round */
+    const resetRound = () => {
+        setLevel(0)
         setScore(0)
         setGameOver(false)
     }
@@ -48,6 +63,7 @@ const App = (props) => {
 
     /* Effect is used to update the saga name, which is dependent on the user selected game mode */
     useEffect(() => {
+        setModeScore(scoreList[mode])
         if (mode == 'full') {
             setSagaName(sagaList[level])
         } else if (mode == 'z') { 
@@ -66,7 +82,18 @@ const App = (props) => {
     
     /* Effect is used to update the high score, which is dependent on the current score changing */
     useEffect(() => {
-        if (highScore < score) { setHighScore(score) }
+        if (mode == 'full') {
+            if (allHS < score) { setAllHS(score) }
+        } else if (mode == 'z') { 
+            if (dbzHS < score) { setDBZHS(score) }
+        } else if (mode == 'super') {
+            if (dbsHS < score) { setDBSHS(score) }
+        } else if (mode == 'movies') {
+            if (dbmHS < score) { setDBMHS(score) }
+        } else if (mode == 'gt') {
+            if (dbgtHS < score) { setDBGTHS(score) }
+        }
+
         if (score == scoreList[mode]) { setGameOver(true) }
     }, [score])
 
@@ -91,7 +118,11 @@ const App = (props) => {
             {
                 sagaName != 'Home' && 
                 !gameOver &&
-                <Header highScore={highScore} sagaName={sagaName} score={score}/>
+                <Header 
+                    sagaName={sagaName}
+                    score={score}
+                    highScore= {mode == 'full' ? allHS : mode == 'z' ? dbzHS : mode == 'super' ? dbsHS : mode == 'gt' ? dbgtHS : mode == 'movies' ? dbmHS : 0}
+                />
             }
 
             {
@@ -108,7 +139,14 @@ const App = (props) => {
 
             {
                 gameOver && 
-                <GameOver resetGame={resetGame} highScore={highScore} />
+                <GameOver
+                    mode={mode}
+                    modeScore={modeScore}
+                    resetGame={resetGame}
+                    resetRound={resetRound}
+                    sagaName={sagaName}
+                    score={score} 
+                />
             }
 
             <svg className='volume-switch' onClick={volumeSwitch} style={{display: isPlaying == true ? 'block' : 'none'}} width='50px' height='50px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><g id='SVGRepo_bgCarrier' strokeWidth='0'></g><g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g><g id='SVGRepo_iconCarrier'> <path d='M16.0004 9.00009C16.6281 9.83575 17 10.8745 17 12.0001C17 13.1257 16.6281 14.1644 16.0004 15.0001M18 5.29177C19.8412 6.93973 21 9.33459 21 12.0001C21 14.6656 19.8412 17.0604 18 18.7084M4.6 9.00009H5.5012C6.05213 9.00009 6.32759 9.00009 6.58285 8.93141C6.80903 8.87056 7.02275 8.77046 7.21429 8.63566C7.43047 8.48353 7.60681 8.27191 7.95951 7.84868L10.5854 4.69758C11.0211 4.17476 11.2389 3.91335 11.4292 3.88614C11.594 3.86258 11.7597 3.92258 11.8712 4.04617C12 4.18889 12 4.52917 12 5.20973V18.7904C12 19.471 12 19.8113 11.8712 19.954C11.7597 20.0776 11.594 20.1376 11.4292 20.114C11.239 20.0868 11.0211 19.8254 10.5854 19.3026L7.95951 16.1515C7.60681 15.7283 7.43047 15.5166 7.21429 15.3645C7.02275 15.2297 6.80903 15.1296 6.58285 15.0688C6.32759 15.0001 6.05213 15.0001 5.5012 15.0001H4.6C4.03995 15.0001 3.75992 15.0001 3.54601 14.8911C3.35785 14.7952 3.20487 14.6422 3.10899 14.4541C3 14.2402 3 13.9601 3 13.4001V10.6001C3 10.04 3 9.76001 3.10899 9.54609C3.20487 9.35793 3.35785 9.20495 3.54601 9.10908C3.75992 9.00009 4.03995 9.00009 4.6 9.00009Z' stroke='white' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'></path> </g></svg>
