d08de62c075bd27cae53c98b85de0041f1cd334d
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-09 18:59:23 -0500

    Added character list for the Dragon Ball GT Baby Saga

---
+++ src/characters/babysaga.js
@@ -0,0 +1,380 @@
+import { nanoid } from 'nanoid'
+
+const babysaga = [
+    [
+        {
+            id: nanoid(),
+            name: 'Super Sigma 3',
+            title: 'Mechanical Fusion',
+            img: 'src/assets/supersigma3.png',
+            type: 'int',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Hyper Meta-Rilldo',
+            title: 'Fierce Focused Attack',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015240/1015240.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: 'Trusted by Friends',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014480/1014480.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'General Rilldo',
+            title: 'Mighty Machine Mutant',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005920/1005920.png',
+            type: 'str',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku (GT)',
+            title: 'The All-Out Release',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005770/1005770.png',
+            type: 'steq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Hyper Meta-Rilldo',
+            title: 'Hyper-Powerful Foe',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025140/1025140.png',
+            type: 'estr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Meta-Rilldo',
+            title: 'Planet of Steel',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005940/1005940.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Pan (GT)',
+            title: 'Impassioned Strike',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005870/1005870.png',
+            type: 'teq',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Baby (Youth Form)',
+            title: 'Wicked Tuffle',
+            img: 'src/assets/babyyouth.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Dr. Myuu',
+            title: 'Artificial Life-Maker',
+            img: 'src/assets/drmyuu.png',
+            type: 'agl',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (GT) & Pan (GT) & Trunks (GT)',
+            title: 'Endless Adventure',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016370/1016370.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Meta-Rilldo',
+            title: 'Metal-Clad Foe',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022000/1022000.png',
+            type: 'estr',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Baby (Youth Form)',
+            title: 'Wicked Life Form',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006000/1006000.png',
+            type: 'int',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (GT)',
+            title: 'Sound Judgment',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006250/1006250.png',
+            type: 'agl',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goten (GT)',
+            title: 'Top-Notch Strength',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022030/1022030.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goten Possessed (GT)',
+            title: 'Ambitious First Step',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006280/1006280.png',
+            type: 'phy',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta (GT)',
+            title: 'Eternal Rival',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1004040/1004040.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta (GT)',
+            title: 'Reignited Fighting Spirit',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003000/1003000.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'SS Goten Possessed (GT)',
+            title: 'Inception of Evil',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006310/1006310.png',
+            type: 'ephy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'SS Gohan Possessed (GT)',
+            title: 'Viciousness Released',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006300/1006300.png',
+            type: 'eteq',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: 'Undying Superpower',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007300/1007300.png',
+            type: 'teq',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Baby Vegeta',
+            title: 'Dreadful Fusion',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006010/1006010.png',
+            type: 'eagl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goten Possessed (GT)',
+            title: 'Ambitious First Step',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006280/1006280.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan Possessed (GT)',
+            title: 'Evil Transferred',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006270/1006270.png',
+            type: 'teq',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Goku (GT)',
+            title: 'Focused on Victory',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1003040/1003040.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Baby Vegeta',
+            title: 'Most Calamitous Revenge Ever',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016380/1016380.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Baby 1',
+            title: 'Corroding Menace',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006020/1006020.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Baby 2',
+            title: 'Revenge Exacted',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006030/1006030.png',
+            type: 'int',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Uub',
+            title: 'Hidden Majin Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4016320/4016320.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Uub (Teen)',
+            title: 'Hidden Majin Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016310/1016310.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Goku (GT) (Golden Giant Ape)',
+            title: 'Inklings of Ultimate Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008290/1008290.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Baby 2 (Giant Ape)',
+            title: 'Rampaging Vengeance',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006520/1006520.png',
+            type: 'eint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 4 Goku',
+            title: 'Scarlet Hero',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009420/1009420.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Pan (GT)',
+            title: 'Furious Girl',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009990/1009990.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 3 Goku (GT) (Golden Giant Ape)',
+            title: 'Inklings of Ultimate Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4008290/4008290.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Baby 2 (Giant Ape)',
+            title: 'Ultimate Malign Being',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016400/1016400.png',
+            type: 'eagl',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 4 Goku',
+            title: 'Evolution of the Highest Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026900/1026900.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Trunks (GT)',
+            title: 'Brains and Brawn Combined',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010020/1010020.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Uub',
+            title: 'Merging Destinies',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1004000/1004000.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Baby 2 (Giant Ape)',
+            title: 'Rampaging Vengeance',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4006520/4006520.png',
+            type: 'eint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 4 Goku & Super Uub',
+            title: 'Evolution of the Highest Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4026910/4026910.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Full Power Saiyan 4 Goku',
+            title: 'Evolution of the Highest Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4026920/4026920.png',
+            type: 'sint',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Baby',
+            title: 'Evil Intelligence and Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026870/1026870.png',
+            type: 'eteq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Baby 2 (Giant Ape)',
+            title: 'Ultimate Malign Being',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4016400/4016400.png',
+            type: 'eagl',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default babysaga
