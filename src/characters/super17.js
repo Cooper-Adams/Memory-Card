f2b4a43b1e5b96cb7ef744d34398891fc996fdf2
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-11 20:00:08 -0400

    Added character list for the Dragon Ball GT Super Android 17 Saga

---
+++ src/characters/super17.js
@@ -0,0 +1,278 @@
+import { nanoid } from 'nanoid'
+
+const super17 = [
+    [
+        {
+            id: nanoid(),
+            name: 'Dr. Myuu & Dr. Gero',
+            title: 'Collusion Between Genius Scientists',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026220/1026220.png',
+            type: 'phy',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #17',
+            title: 'Blackened Heart',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010520/1010520.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Hell Fighter #17',
+            title: 'Unified Desire of Vengeance',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016990/1016990.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (GT)',
+            title: 'Exceptional Strength',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022660/1022660.png',
+            type: 'agl',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Android #17',
+            title: 'Hell and Earth in Unison',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010540/1010540.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Hell Fighter #17',
+            title: 'Message from Hell',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010550/1010550.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Dr. Myuu & Dr. Gero',
+            title: 'Avengers from Hell',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026230/1026230.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: 'Undying Superpower',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007300/1007300.png',
+            type: 'teq',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Trunks (GT) & Goten (GT)',
+            title: 'Sworn Friends',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022020/1022020.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #19',
+            title: 'Engineered Evil',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1001830/1001830.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Uub',
+            title: 'Powers Combined',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005750/1005750.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Pui Pui',
+            title: 'Confident Sentry',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1002070/1002070.png',
+            type: 'agl',
+            rarity: 'r'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Pan (GT)',
+            title: "Spirited Girl's Efforts",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1024980/1024980.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Staff Officer Black',
+            title: 'Commander of the Red Ribbon Army',
+            img: 'src/assets/staffofficerblack.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'General Blue',
+            title: 'Old Foe Returned',
+            img: 'src/assets/generalblue.png',
+            type: 'agl',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Major Metallitron',
+            title: 'Long Forgotten Foe',
+            img: 'src/assets/metallitron.png',
+            type: 'str',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Cell (Perfect Form) (GT) & Freeza (Final Form) (GT)',
+            title: 'Whirling Grudge in the Pit of Hell',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027470/1027470.png',
+            type: 'ephy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: 'Trusted by Friends',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014480/1014480.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'General Rilldo',
+            title: 'Might Machine Mutant',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005920/1005920.png',
+            type: 'str',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (GT)',
+            title: 'Sound Judgment',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006250/1006250.png',
+            type: 'agl',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Hell Fighter #17',
+            title: 'Inorganic Murderous Intent',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027520/1027520.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta (GT)',
+            title: 'Never-Ending Challenge',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021930/1021930.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #17',
+            title: 'Ruled by Bloodlust',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010560/1010560.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #18 (GT)',
+            title: 'Furious Assault',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1005810/1005810.png',
+            type: 'str',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku (GT)',
+            title: 'Battle of Epic Proportions',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1009950/1009950.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta (GT)',
+            title: 'Lone Guardian',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010030/1010030.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #17 & Hell Fighter #17',
+            title: 'Evil-Stained Androids',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026200/1026200.png',
+            type: 'estr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super #17',
+            title: 'Ceremony of Revenge',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022090/1022090.png',
+            type: 'eteq',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: 'Hidden Majin Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014490/1014490.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Android #18 (GT)',
+            title: 'Impulse-Driven Heartbreak and Anger',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027510/1027510.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 4 Goku',
+            title: 'No Holds Barred',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015750/1015750.png',
+            type: 'sagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super #17',
+            title: 'Evil-Stained Androids',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4026210/4026210.png',
+            type: 'estr',
+            rarity: 'ur'
+        }
+    ]
+]
+
+export default super17
