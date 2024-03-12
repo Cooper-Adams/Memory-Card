53f71e3cfc317f75b5e6386744184f339012c190
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-12 00:16:41 -0400

    Updated final saga scores

--- src/characters/_sagascores.js
+++ src/characters/_sagascores.js
@@ -1,8 +1,9 @@
 let scoreList = {}
 
-scoreList['full'] = 556 //TEMP UNTIL ALL SAGAS FINISHED
+scoreList['full'] = 672
 scoreList['dbz'] = 188
 scoreList['dbs'] = 216
+scoreList['gt'] = 116
 scoreList['movies'] = 188
 
 export default scoreList
