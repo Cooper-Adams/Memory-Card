ead1c27cee666217496f36679a16d1a487bd6491
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-12 00:17:40 -0400

    Added character list for the Dragon Ball GT Shadow Dragon Saga

---
+++ src/characters/shadowdragon.js
@@ -0,0 +1,244 @@
+import { nanoid } from 'nanoid'
+
+const shadowdragon = [
+    [
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: 'Undying Superpower',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1007300/1007300.png',
+            type: 'teq',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Haze Shenron',
+            title: 'Noxious Evil',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006340/1006340.png',
+            type: 'teq',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Giru',
+            title: 'Grand Tour Companion',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016300/1016300.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Pan (GT)',
+            title: 'Victorious Smile',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015230/1015230.png',
+            type: 'sagl',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Rage Shenron',
+            title: 'Electric Slime',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006370/1006370.png',
+            type: 'phy',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Naturon Shenron',
+            title: 'Earthshaking Confidence',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006400/1006400.png',
+            type: 'int',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Oceanus Shenron',
+            title: 'Dominance over the Atmosphere',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006380/1006380.png',
+            type: 'agl',
+            rarity: 'r'
+        },
+        {
+            id: nanoid(),
+            name: 'Pan (GT)',
+            title: "Talent's in the Blood",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010000/1010000.png',
+            type: 'sphy',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Nuova Shenron',
+            title: 'Warrior of the Sun',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006360/1006360.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 4 Goku',
+            title: 'Miraculous Awakening',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1015740/1015740.png',
+            type: 'agl',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Eis Shenron',
+            title: 'Ice-Cold Warrior',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006350/1006350.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Syn Shenron',
+            title: "Shadow Dragons' Master",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008720/1008720.png',
+            type: 'phy',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Full Power Saiyan 4 Goku',
+            title: 'Hope-Filled Strike',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1014130/1014130.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Syn Shenron',
+            title: 'Counterattack of the Dragon Balls',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023580/1023580.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (GT) & Goten (GT)',
+            title: 'Earth-Protecting Fraternal Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022040/1022040.png',
+            type: 'steq',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Vegeta (GT)',
+            title: 'Limit-Breaking Challenge',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021390/1021390.png',
+            type: 'sint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 4 Goku & Super Saiyan 4 Vegeta',
+            title: 'Saiyan Warriors with Ultimate Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022420/1022420.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Syn Shenron',
+            title: 'Agent of Destruction',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1006330/1006330.png',
+            type: 'ephy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Omega Shenron',
+            title: 'Hopeless Minus Energy',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008710/1008710.png',
+            type: 'estr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 4 Gogeta',
+            title: 'Peerless Gleam',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1011300/1011300.png',
+            type: 'steq',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan 4 Goku & Super Saiyan 4 Vegeta',
+            title: 'Transcendent Battle',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022410/1022410.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (GT) & Super Saiyan 4 Vegeta',
+            title: 'Will to Confront Destruction',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1025760/1025760.png',
+            type: 'sstr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Omega Shenron',
+            title: 'The Ultimate Evil Dragon',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027040/1027040.png',
+            type: 'eagl',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Nuova Shenron',
+            title: 'Flaming Avenger',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1020970/1020970.png',
+            type: 'sphy',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Omega Shenron',
+            title: 'Outpouring of Evil Energy',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1027030/1027030.png',
+            type: 'eagl',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta (GT)',
+            title: 'Miracle-Making Final Showdown',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4025780/4025780.png',
+            type: 'sstr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Goku (GT)',
+            title: 'Culmination of Hope',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016580/1016580.png',
+            type: 'sint',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku Jr. & Super Saiyan Vegeta Jr.',
+            title: 'Legends of a New Generation',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1023860/1023860.png',
+            type: 'sagl',
+            rarity: 'lr'
+        }
+    ]
+]
+
+export default shadowdragon
