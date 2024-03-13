6b60be256c17ee59f55f06c4d541fd2934fb9cdf
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-13 14:53:12 -0400

    Fixed picture references

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,9 +17,9 @@ const Card = (props) => {
     return (
         <div className='card-container'>
             <div className={props.type + ' card'} onClick={handleClick}>
-                <img draggable="false" className='card-rarity' src={'/src/assets/' + props.rarity + '.png'} />
-                <img draggable="false" className='card-type' src={'/src/assets/' + props.type + '.png'} />
-                <img draggable="false" className='card-img' src={props.img} />
+                <img draggable='false' className='card-rarity' src={'/' + props.rarity + '.png'} />
+                <img draggable='false' className='card-type' src={'/' + props.type + '.png'} />
+                <img draggable='false' className='card-img' src={props.img} />
             </div>
 
             <div className={props.type + ' card-nt'}>
