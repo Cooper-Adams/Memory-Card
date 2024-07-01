2e8e0ffe5ae82e952fefd2f1f646e5f8e5ba9d99
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-07-01 15:13:56 -0400

    Revert "Fixed picture references"
    This reverts commit e83a8aedbf5376b6df867f1a46405bfe231b00fe.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,8 +17,8 @@ const Card = (props) => {
     return (
         <div className='card-container'>
             <div className={props.type + ' card'} onClick={handleClick}>
-                <img draggable='false' className='card-rarity' src={'public/' + props.rarity + '.png'} />
-                <img draggable='false' className='card-type' src={'public/' + props.type + '.png'} />
+                <img draggable='false' className='card-rarity' src={props.rarity + '.png'} />
+                <img draggable='false' className='card-type' src={props.type + '.png'} />
                 <img draggable='false' className='card-img' src={props.img} />
             </div>
 
