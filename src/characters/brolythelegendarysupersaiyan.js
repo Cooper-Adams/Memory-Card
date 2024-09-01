08942ca64eb2e4889756c8f9e57a025111a08e23
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-08-31 20:13:45 -0400

    Updated with new WWDC characters

--- src/characters/brolythelegendarysupersaiyan.js
+++ src/characters/brolythelegendarysupersaiyan.js
@@ -5,17 +5,17 @@ const lssjbroly = [
         {
             id: nanoid(),
             name: 'Paragus',
-            title: 'Ambitious Scheme',
-            img: 'https://dokkaninfo.com/assets/japan/character/card/2000760/2000760.png',
-            type: 'phy',
-            rarity: 'sr'
+            title: 'Plot on New Planet Vegeta',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029630/1029630.png',
+            type: 'teq',
+            rarity: 'ssr'
         },
         {
             id: nanoid(),
-            name: 'Super Saiyan Broly',
-            title: 'Uncontrollable Power',
-            img: 'https://dokkaninfo.com/assets/japan/character/card/1021660/1021660.png',
-            type: 'int',
+            name: 'Broly',
+            title: 'Saiyan of a Tragic Fate',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029380/1029380.png',
+            type: 'teq',
             rarity: 'ssr'
         },
         {
@@ -46,11 +46,11 @@ const lssjbroly = [
         },
         {
             id: nanoid(),
-            name: 'Super Saiyan Goku',
-            title: 'Eruption of Golden Ki',
-            img: 'https://dokkaninfo.com/assets/japan/character/card/1022960/1022960.png',
-            type: 'sstr',
-            rarity: 'ur'
+            name: 'Super Saiyan Broly',
+            title: 'Uncontrollable Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1021660/1021660.png',
+            type: 'int',
+            rarity: 'ssr'
         },
         {
             id: nanoid(),
@@ -62,10 +62,44 @@ const lssjbroly = [
         },
         {
             id: nanoid(),
-            name: 'Super Saiyan Vegeta',
-            title: 'Reclaim the Summit',
-            img: 'https://dokkaninfo.com/assets/japan/character/card/1005550/1005550.png',
+            name: 'Piccolo',
+            title: 'Assistance from Earth',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029760/1029760.png',
+            type: 'phy',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Legendary Super Saiyan Broly',
+            title: 'Universe-Devastating Combat Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4029410/4029410.png',
             type: 'eteq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku',
+            title: 'Fierce Clash of Burning Power',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4029370/4029370.png',
+            type: 'sphy',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goku & Super Saiyan Gohan (Youth) & Super Saiyan Trunks (Teen)',
+            title: 'Fighting Spirit-Heightening Clash',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029350/1029350.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Vegeta',
+            title: 'Resolve to Challenge Powerful Foes',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4029780/4029780.png',
+            type: 'sphy',
             rarity: 'ur'
         }
     ]
