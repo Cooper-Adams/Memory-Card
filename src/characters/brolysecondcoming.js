f1dddd40b797de493ff540d5e965c3f7375971e3
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-09-13 00:16:40 -0400

    Updated Broly Second Coming character list

--- src/characters/brolysecondcoming.js
+++ src/characters/brolysecondcoming.js
@@ -12,6 +12,40 @@ const brolysecondcoming = [
         },
         {
             id: nanoid(),
+            name: 'Goten (Kid)',
+            title: 'Fearless Adventure',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1017520/1017520.png',
+            type: 'phy',
+            rarity: 'sr'
+        },
+        {
+            id: nanoid(),
+            name: 'Videl',
+            title: 'Adventure at Natade Village',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029610/1029610.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Trunks (Kid)',
+            title: 'Flash of Genius',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1008270/1008270.png',
+            type: 'agl',
+            rarity: 'sr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Broly',
+            title: 'Awakened Demonic Warrior',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016850/1016850.png',
+            type: 'teq',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
             name: 'Super Saiyan Goten (Kid)',
             title: 'Curiosity-Filled Struggle',
             img: 'https://dokkaninfo.com/assets/japan/character/card/1017530/1017530.png',
@@ -38,11 +72,19 @@ const brolysecondcoming = [
     [
         {
             id: nanoid(),
-            name: 'Super Saiyan Gohan (Teen) & Super Saiyan Goten (Kid)',
-            title: 'Miracle-Calling Clash',
-            img: 'https://dokkaninfo.com/assets/japan/character/card/1016810/1016810.png',
-            type: 'steq',
-            rarity: 'lr'
+            name: 'Super Saiyan Broly',
+            title: 'Hideous Super Saiyan',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010430/1010430.png',
+            type: 'estr',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Goten (Kid)',
+            title: 'Pint-Sized Saiyan Goes Wild',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1026000/1026000.png',
+            type: 'sphy',
+            rarity: 'ur'
         },
         {
             id: nanoid(),
@@ -54,11 +96,71 @@ const brolysecondcoming = [
         },
         {
             id: nanoid(),
+            name: 'Super Saiyan Trunks (Kid)',
+            title: 'Brilliant Plan to Conquer the Enemy',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029670/1029670.png',
+            type: 'sint',
+            rarity: 'ur'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Teen)',
+            title: 'All-Out Super Attack',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029470/1029470.png',
+            type: 'sstr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Teen)',
+            title: 'Strike for Destiny',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016900/1016900.png',
+            type: 'sphy',
+            rarity: 'ur'
+        },
+        {
+            id: nanoid(),
             name: 'Super Saiyan Broly',
             title: 'Super Saiyan Counterattack',
             img: 'https://dokkaninfo.com/assets/japan/character/card/1016860/1016860.png',
             type: 'eteq',
             rarity: 'ur'
+        },
+        {
+            id: nanoid(),
+            name: 'Legendary Super Saiyan Broly',
+            title: 'Super Warrior Who Destroys All',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029440/1029440.png',
+            type: 'eint',
+            rarity: 'lr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Teen)',
+            title: 'All-Out Super Attack',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/4029480/4029480.png',
+            type: 'sstr',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Super Saiyan Gohan (Teen) & Super Saiyan Goten (Kid)',
+            title: 'Miracle-Calling Clash',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1016810/1016810.png',
+            type: 'steq',
+            rarity: 'lr'
+        },
+        {
+            id: nanoid(),
+            name: 'Legendary Super Saiyan Broly',
+            title: 'Indestructible Saiyan Evil',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1010440/1010440.png',
+            type: 'estr',
+            rarity: 'lr'
         },
         {
             id: nanoid(),
