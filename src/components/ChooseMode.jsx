5c6dcd3eacf62e336bce71480f7ff94cf2ea3f21
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-10-15 13:30:42 -0400

    Added logic to not play the slide in if it has already been played during the session

--- src/components/ChooseMode.jsx
+++ src/components/ChooseMode.jsx
@@ -1,32 +1,35 @@
 import React from 'react'
 
 const ChooseMode = (props) => {
-    const gameMode = (e) => { props.setMode(e.target.title) } 
+    const gameMode = (e) => { 
+        props.setMode(e.target.title)
+        props.setPlayed(true)
+    } 
 
     return (
         <div className='mode-container'>
             <div className='mode-box'>
-                <div className='modal-box'>
+                <div className={'modal-box' + (props.played == false ? ' animate' : '')}>
                     <img onClick={gameMode} title='full' className='modal-img' src='Full.jpg' alt='All Dragon Ball Sagas'/>
                     <h1 className='saga-title'>All Sagas</h1>
                 </div>
 
-                <div className='modal-box'>
+                <div className={'modal-box' + (props.played == false ? ' animate' : '')}>
                     <img onClick={gameMode} className='modal-img' title='z' src='Z.jpg' alt='Sagas from Dragon Ball Z'/>
                     <h1 className='saga-title'>Dragon Ball Z Sagas</h1>
                 </div>
 
-                <div className='modal-box'>
+                <div className={'modal-box' + (props.played == false ? ' animate' : '')}>
                     <img onClick={gameMode} className='modal-img' title='super' src='Super.jpg' alt='Sagas from Dragon Ball Super'/>
                     <h1 className='saga-title'>Dragon Ball Super Sagas</h1>
                 </div>
 
-                <div className='modal-box'>
+                <div className={'modal-box' + (props.played == false ? ' animate' : '')}>
                     <img onClick={gameMode} className='modal-img' title='gt' src='dragonballgt.webp' alt='Sagas from Dragon Ball GT'/>
                     <h1 className='saga-title'>Dragon Ball GT Sagas</h1>
                 </div>
 
-                <div className='modal-box'>
+                <div className={'modal-box' + (props.played == false ? ' animate' : '')}>
                     <img onClick={gameMode} className='modal-img' title='movies' src='Movies.png' alt='Sagas from Dragon Ball Z Movies'/>
                     <h1 className='saga-title'>Dragon Ball Movie Sagas</h1>
                 </div>
