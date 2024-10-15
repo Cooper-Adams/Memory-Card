ac91324a31cb3b0d73dddbec99a708e89fcecf63
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-10-15 13:29:13 -0400

    Added a file that has the names of the osts

---
+++ src/assets/sounds/_musicTitles.js
@@ -0,0 +1,65 @@
+import exterminationplan from './LRStrGoku.webm'
+import bojackunbound from './LRTeqSSJ2Gohan.webm'
+import otherworld from './INTSSJ3Goku.webm'
+import greatsaiyaman from './TEQUltimateGohan.webm'
+import brolysecondcoming from './LRGohanGoten.webm'
+import biobroly from './PHYGotenTrunks.webm'
+import majinbuusaga from './LRPhyGokuVegeta.webm'
+import fusionreborn from './LRTeqGogeta.webm'
+import wrathofthedragon from './STRSSJ3Goku.webm'
+import battleofgods from './PHYGodGoku.webm'
+import resurrectionf from './LRAglGoldenFreeza.webm'
+import universe6vs7 from './LRStrGokuHit.webm'
+import copyvegeta from './LRAglGokuVegetaBlue.webm'
+import futuresaga from './LRTeqVegito.webm'
+import tournamentofpower from './INTUiGoku.webm'
+import superbroly from './LRPhyGogeta.webm'
+import superhero from './LRBeastGohan.webm'
+import blackstar from './PHYSSJGoku.webm'
+import superandroid17 from './STRSuper17.webm'
+import gtduostandby from './LRGTDuoStandby.webm'
+import babysaga from './LRIntSSJ4Goku.webm'
+
+let musicTitles = {}
+
+musicTitles['Home'] = '7th Anniversary Home Screen Theme'
+musicTitles['Dragon Ball'] = "Death Match for World Domination, Piccolo Jr.'s Active Skill Theme"
+musicTitles['Dragon Ball: The Path to Power'] = "Quest for the Dragon Balls, Goku (Youth)'s Active Skill Theme"
+musicTitles['Dragon Ball Z: The Movie - Dead Zone'] = "Rivals Acting in Concert, Goku & Piccolo's Active Skill Theme"
+musicTitles['Saiyan Saga'] = "UPDATE"
+musicTitles["Dragon Ball Z: The Movie - The World's Strongest"] = "Devilish Genius Scientist, Dr Wheelo's Active Skill Theme"
+musicTitles['Dragon Ball Z: The Decisive Battle for the Whole Earth'] = "Power to Defend the Green Planet, Goku's Active Skill Theme"
+musicTitles['Dragon Ball Z: Super Saiyan Son Goku'] = "Battle Against Limits, Goku (Kaioken)'s Active Skill Theme"
+musicTitles['Planet Namek Saga'] = "Strike Full of Anger, Super Saiyan Goku's Active Skill Theme"
+musicTitles['Dragon Ball Z: A Lonesome, Final Battle - The Father of Z Warrior Kakarot, who Challenged Freeza'] = "UPDATE"
+musicTitles['Dragon Ball Z: The Outstanding Strongest vs. Strongest'] = "Extraordinary Super Warrior, Super Saiyan Goku's Revival Theme"
+musicTitles['Dragon Ball Z: Clash!! 10 Billion Power Warriors'] = "Full-Power Final Showdown, Super Saiyan Goku & Super Saiyan Vegeta's Active Skill Theme"
+musicTitles['Androids Saga'] = "UPDATE"
+musicTitles['Dragon Ball Z: Extreme Battle!! Three Great Super Saiyans'] = "All-Out Ultimate Battle, Super Saiyan Goku & Super Saiyan Vegeta & Super Saiyan Trunks (Teen)'s Active Skill Theme"
+musicTitles['Dragon Ball Z: Defiance in the Face of Despair!! The Remaining Super-Warriors: Gohan and Trunks'] = "Remaining Hope, Super Saiyan Gohan (Future)'s Revival Skill Theme"
+musicTitles['Dragon Ball Z: Burn!! Hot Battle - Fierce Battle - Super Fierce Battle'] = "UPDATE"
+musicTitles['Cell Saga'] = "Warriors Entrusted with Earth's Fate, Super Saiyan Goku/Super Saiyan Gohan (Youth)'s Exchange Skill Theme"
+musicTitles['Trunks of the Future Timeline'] = "The Result of Time Traveling, Trunks (Teen) (Future)'s Active Skill Theme"
+musicTitles['Dragon Ball Z Gaiden: Saiya-jin Zetsumetsu Keikaku'] = "Earth-Shaking Immense Power, Super Saiyan Goku's Active Skill Theme"
+musicTitles['Dragon Ball Z: The Last Minute of the Galaxy!! A Super Incredible Guy'] = "Earth-Protecting Flash, Super Saiyan 2 Gohan (Youth)'s Active Skill Theme"
+musicTitles['Other World Saga'] = otherworld
+musicTitles['Great Saiyaman Saga'] = greatsaiyaman
+musicTitles["Dragon Ball Z: Two Dangerous People! Super Warriors Can't Sleep"] = brolysecondcoming
+musicTitles["Dragon Ball Z Defeat the Super Warrior!! I'll Be The Winner"] = biobroly
+musicTitles['Majin Buu Saga'] = majinbuusaga
+musicTitles['Dragon Ball Z: Resurrection Fusion!! Goku and Vegeta'] = fusionreborn
+musicTitles["Dragon Ball Z: Dragon Fist Explosion!! Who Will Do It If Goku Doesn't Do It?"] = wrathofthedragon
+musicTitles['Dragon Ball Z: Battle of Gods'] = battleofgods
+musicTitles["Dragon Ball Z: Resurrection 'F'"] = resurrectionf
+musicTitles['Universe 6 Saga'] = universe6vs7
+musicTitles['Copy-Vegeta Saga'] = copyvegeta
+musicTitles['Future Saga'] = futuresaga
+musicTitles['Universe Survival Saga'] = tournamentofpower
+musicTitles['Dragon Ball Super: Broly'] = superbroly
+musicTitles['Dragon Ball Super: Super Hero'] = superhero
+musicTitles['Black Star Dragon Ball Saga'] = blackstar
+musicTitles['Baby Saga'] = babysaga
+musicTitles['Super Android 17 Saga'] = superandroid17
+musicTitles['Shadow Dragon Saga'] = gtduostandby
+
+export default musicTitles
