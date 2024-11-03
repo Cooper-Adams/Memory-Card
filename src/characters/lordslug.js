7fd0b3d73de542ef5f54b97db657f25a28a57cbc
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-11-03 13:05:30 -0500

    Updated with new characters

--- src/characters/lordslug.js
+++ src/characters/lordslug.js
@@ -4,6 +4,40 @@ const lordslug = [
     [
         {
             id: nanoid(),
+            name: 'Slug',
+            title: 'Obsession with Youth',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029720/1029720.png',
+            type: 'str',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Angila & Medamatcha',
+            title: 'Demons from Outer Space',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1029740/1029740.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Gohan (Kid)',
+            title: 'Wings of Wonder',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1013740/1013740.png',
+            type: 'int',
+            rarity: 'ssr'
+        },
+        {
+            id: nanoid(),
+            name: 'Piccolo',
+            title: "Demon's Intuition",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1022300/1022300.png',
+            type: 'str',
+            rarity: 'ssr'
+        }
+    ],
+    [
+        {
+            id: nanoid(),
             name: 'Slug (Giant Form)',
             title: 'Evil Namekian',
             img: 'https://dokkaninfo.com/assets/japan/character/card/1013710/1013710.png',
@@ -21,18 +55,18 @@ const lordslug = [
         {
             id: nanoid(),
             name: 'Gohan (Kid)',
-            title: 'Wings of Wonder',
-            img: 'https://dokkaninfo.com/assets/japan/character/card/1013740/1013740.png',
-            type: 'int',
-            rarity: 'ssr'
+            title: 'Fair Weather and Sunny Soul',
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1030220/1030220.png',
+            type: 'sint',
+            rarity: 'ur'
         },
         {
             id: nanoid(),
-            name: 'Piccolo',
-            title: "Demon's Intuition",
-            img: 'https://dokkaninfo.com/assets/japan/character/card/1022300/1022300.png',
-            type: 'str',
-            rarity: 'ssr'
+            name: 'Krillin',
+            title: "Tide-Turning Veteran",
+            img: 'https://dokkaninfo.com/assets/japan/character/card/1030060/1030060.png',
+            type: 'sint',
+            rarity: 'ur'
         }
     ]
 ]
